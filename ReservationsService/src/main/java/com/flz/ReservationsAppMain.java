cceb27a6602d76268c72cb154294ac0e2a430faf
filizhelvaci <filizhelvaci82@gmail.com>
2024-05-15 20:11:58 +0300

    Controller, service, repository created for custumers

---
+++ ReservationsService/src/main/java/com/flz/ReservationsAppMain.java
@@ -0,0 +1,12 @@
+package com.flz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsAppMain {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReservationsAppMain.class, args);
+    }
+}
--- ReservationsService/src/main/java/com/flz/model/reservation.java
+++ ReservationsService/src/main/java/com/flz/model/reservation.java
@@ -1,13 +1,15 @@
 package com.flz.model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import java.util.Date;
+
 
 public class reservation {
     private String roomId;
     private String userId;
 
     private Date startDate;
-
     private Date exitDate;
 
 }
