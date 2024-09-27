e002646e324d66da5533c4790ece06afe90434d2
filizhelvaci <filizhelvaci82@gmail.com>
2024-09-27 12:24:23 +0300

    created sql tables

--- RoomService/src/main/resources/rooms.sql
+++ RoomService/src/main/resources/rooms.sql
@@ -1,7 +1,3 @@
-create database hotel_database;
-
-create schema hotel_room;
-
 create table hotel_room.rs_room_type
 (
     id           bigint generated always as identity primary key,
@@ -9,7 +5,11 @@ create table hotel_room.rs_room_type
     price        numeric(14, 4) not null,
     person_count smallint       not null,
     size         smallint  not null,
-    description  varchar(200)
+    description  varchar(200),
+    created_at timestamp(0) not null default current_timestamp,
+    created_by varchar(120) not null ,
+    update_at timestamp(0) not null default current_timestamp,
+    update_by varchar(120) not null
 );
 create table hotel_room.rs_room
 (
@@ -17,7 +17,11 @@ create table hotel_room.rs_room
     type_id bigint      not null references rs_room_type (id),
     number  smallint    not null,
     floor   smallint    not null,
-    status  varchar(25) not null check (status in ('EMPTY', 'FULL', 'RESERVE', 'IN_MAINTENANCE'))
+    status  varchar(25) not null check (status in ('EMPTY', 'FULL', 'RESERVE', 'IN_MAINTENANCE')),
+    created_at timestamp(0) not null default current_timestamp,
+    created_by varchar(120) not null ,
+    update_at timestamp(0) not null default current_timestamp,
+    update_by varchar(120) not null
 );
 
 
@@ -27,13 +31,20 @@ create table hotel_room.rs_asset
     id         bigint generated always as identity primary key,
     name       varchar(50)    not null,
     price      numeric(14, 4) not null,
-    is_default smallint       not null
+    is_default smallint       not null,
+    created_at timestamp(0) not null default current_timestamp,
+    created_by varchar(120) not null ,
+    update_at timestamp(0) not null default current_timestamp,
+    update_by varchar(120) not null
 );
 
 create table hotel_room.rs_room_type_asset
 (
     id           bigint generated always as identity primary key,
     room_type_id bigint not null references rs_room_type (id),
-    asset_id     bigint not null references rs_asset (id)
-
+    asset_id     bigint not null references rs_asset (id),
+    created_at timestamp(0) not null default current_timestamp,
+    created_by varchar(120) not null ,
+    update_at timestamp(0) not null default current_timestamp,
+    update_by varchar(120) not null
 );
