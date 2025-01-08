c7a3b40f9b6646751252c5bd74887ed613fe961a
Filizkh <filizhelvaci82@gmail.com>
2025-01-08 10:38:17 +0300

    RoomCreateRequest created

---
+++ RoomService/src/main/java/com/flz/model/request/RoomCreateRequest.java
@@ -0,0 +1,24 @@
+package com.flz.model.request;
+
+import com.flz.model.enums.RoomStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@Setter
+public class RoomCreateRequest {
+
+    @NotNull
+    @Range(min = 1, max = 10000)
+    private Integer number;
+
+    @NotNull
+    @Range(min = 0, max = 100)
+    private Integer floor;
+
+    @NotNull
+    private RoomStatus status;
+
+}
