d156ac587a0d4adfb7d95154976c7514ef707347
Filizkh <filizhelvaci82@gmail.com>
2025-01-08 14:18:31 +0300

    RoomController created

---
+++ RoomService/src/main/java/com/flz/controller/RoomController.java
@@ -0,0 +1,71 @@
+package com.flz.controller;
+
+import com.flz.model.request.RoomCreateRequest;
+import com.flz.model.request.RoomUpdateRequest;
+import com.flz.model.response.HotelResponse;
+import com.flz.model.response.RoomResponse;
+import com.flz.model.response.RoomsSummaryResponse;
+import com.flz.service.RoomService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+class RoomController {
+
+    private final RoomService roomService;
+
+    @GetMapping("/rooms/summary")
+    public HotelResponse<List<RoomsSummaryResponse>> findSummaryAll() {
+        final List<RoomsSummaryResponse> roomsSummaryResponse = roomService.findSummaryAll();
+        return HotelResponse.successOf(roomsSummaryResponse);
+    }
+
+    @GetMapping("/room/{id}")
+    public HotelResponse<RoomResponse> findById(@PathVariable(value = "id") Long id) {
+        RoomResponse roomResponse = roomService.findById(id);
+        return HotelResponse.successOf(roomResponse);
+    }
+
+//    @PostMapping("/assets")
+//    public HotelResponse<Page<AssetsResponse>> findAll(@RequestParam(required = false) String name,
+//                                                       @RequestParam(required = false) BigDecimal minPrice,
+//                                                       @RequestParam(required = false) BigDecimal maxPrice,
+//                                                       @RequestParam(defaultValue = "0") int page,
+//                                                       @RequestParam(defaultValue = "10") int size,
+//                                                       @RequestParam(defaultValue = "id") String sortBy,
+//                                                       @RequestParam(defaultValue = "asc") String sortDirection) {
+//        final Page<AssetsResponse> assetsResponses = assetService.findAll(name, minPrice, maxPrice, page, size, sortBy, sortDirection);
+//        return HotelResponse.successOf(assetsResponses);
+//    }
+
+    @PostMapping("/room")
+    public HotelResponse<Void> create(@RequestBody @Valid RoomCreateRequest createRequest) {
+        roomService.create(createRequest);
+        return HotelResponse.success();
+    }
+
+    @PutMapping("/room/{id}")
+    public HotelResponse<Void> update(@PathVariable(value = "id") @Positive Long id, @RequestBody @Valid RoomUpdateRequest roomUpdateRequest) {
+        roomService.update(id, roomUpdateRequest);
+        return HotelResponse.success();
+    }
+
+    @DeleteMapping("/asset/{id}")
+    public HotelResponse<Void> delete(@PathVariable(value = "id") Long id) {
+        roomService.delete(id);
+        return HotelResponse.success();
+    }
+}
