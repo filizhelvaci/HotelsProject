fb5fcf2a47e58a25ef5dee05210b02252c14cbbb
Filizkh <filizhelvaci82@gmail.com>
2025-01-11 18:14:42 +0300

    Room Service updated

--- RoomService/src/main/java/com/flz/service/RoomService.java
+++ RoomService/src/main/java/com/flz/service/RoomService.java
@@ -3,20 +3,11 @@ package com.flz.service;
 import com.flz.model.request.RoomCreateRequest;
 import com.flz.model.request.RoomUpdateRequest;
 import com.flz.model.response.RoomResponse;
-import com.flz.model.response.RoomsResponse;
 import com.flz.model.response.RoomsSummaryResponse;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface RoomService {
-    Page<RoomsResponse> findAll(Integer number,
-                                Integer minNumber,
-                                Integer maxNumber,
-                                int page,
-                                int size,
-                                String sortBy,
-                                String sortDirection);
 
     List<RoomsSummaryResponse> findSummaryAll();
 
