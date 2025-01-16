889fce8113ae53e3b79098a261f03823a38feb13
Filizkh <filizhelvaci82@gmail.com>
2025-01-16 12:47:52 +0300

    in @OneToOne fetch removed

--- RoomService/src/main/java/com/flz/model/entity/RoomEntity.java
+++ RoomService/src/main/java/com/flz/model/entity/RoomEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -47,7 +46,7 @@ public class RoomEntity extends BaseEntity {
       RoomEntity      RoomTypeEntity
           1                 1
     */
-    @OneToOne(fetch = FetchType.EAGER)
+    @OneToOne
     @JoinColumn(name = "room_type_id")
     private RoomTypeEntity type;
 
