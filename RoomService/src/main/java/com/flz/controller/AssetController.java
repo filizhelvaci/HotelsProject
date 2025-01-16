b7a1bb9f02246cfcd80b6818bfade671ccdbcbdb
Filizkh <filizhelvaci82@gmail.com>
2025-01-16 14:01:48 +0300

    Specification implemented in AssetController

--- RoomService/src/main/java/com/flz/controller/AssetController.java
+++ RoomService/src/main/java/com/flz/controller/AssetController.java
@@ -11,6 +11,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,11 +50,14 @@ class AssetController {
     public HotelResponse<Page<AssetsResponse>> findAll(@RequestParam(required = false) String name,
                                                        @RequestParam(required = false) BigDecimal minPrice,
                                                        @RequestParam(required = false) BigDecimal maxPrice,
-                                                       @RequestParam(defaultValue = "0") int page,
-                                                       @RequestParam(defaultValue = "10") int size,
-                                                       @RequestParam(defaultValue = "id") String sortBy,
-                                                       @RequestParam(defaultValue = "asc") String sortDirection) {
-        final Page<AssetsResponse> assetsResponses = assetService.findAll(name, minPrice, maxPrice, page, size, sortBy, sortDirection);
+                                                       @RequestParam(required = false) Boolean isDefault,
+                                                       @RequestParam int page,
+                                                       @RequestParam int size,
+                                                       @RequestParam String property,
+                                                       @RequestParam Sort.Direction direction) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.by(property).with(direction)));
+        Page<AssetsResponse> assetsResponses = assetService.getFilteredAssets(name, minPrice, maxPrice, isDefault, pageable);
         return HotelResponse.successOf(assetsResponses);
     }
 
