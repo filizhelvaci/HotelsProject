68cc3525d1fd547d843a6fbf0d477ae2f2b6a8e5
Filizkh <filizhelvaci82@gmail.com>
2025-01-17 14:36:04 +0300

    Specification added in RoomTypeService

--- RoomService/src/main/java/com/flz/service/RoomTypeService.java
+++ RoomService/src/main/java/com/flz/service/RoomTypeService.java
@@ -6,23 +6,18 @@ import com.flz.model.response.RoomTypeResponse;
 import com.flz.model.response.RoomTypesResponse;
 import com.flz.model.response.RoomTypesSummaryResponse;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface RoomTypeService {
 
-    Page<RoomTypesResponse> findAll(String name,
-                                    BigDecimal minPrice,
-                                    BigDecimal maxPrice,
-                                    int page,
-                                    int size,
-                                    String sortBy,
-                                    String sortDirection);
-
     List<RoomTypesSummaryResponse> findSummaryAll();
 
     RoomTypeResponse findById(Long id);
+
+    Page<RoomTypesResponse> getFilteredRoomsTypes(String name, BigDecimal minPrice, BigDecimal maxPrice, Integer personCount, Integer size, Pageable pageable);
 
     void create(RoomTypeCreateRequest roomTypeCreateRequest);
 
