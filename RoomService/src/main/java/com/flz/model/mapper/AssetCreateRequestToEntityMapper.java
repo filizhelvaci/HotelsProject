79d818cf2810772e2b221311f364a0fda2605c52
Filizkh <filizhelvaci82@gmail.com>
2025-01-27 14:44:05 +0300

    MapStruct used For AssetCreateRequestToEntityMapper

--- RoomService/src/main/java/com/flz/model/mapper/AssetCreateRequestToEntityMapper.java
+++ RoomService/src/main/java/com/flz/model/mapper/AssetCreateRequestToEntityMapper.java
@@ -3,20 +3,12 @@ package com.flz.model.mapper;
 import com.flz.model.entity.AssetEntity;
 import com.flz.model.request.AssetCreateRequest;
 
-import java.time.LocalDateTime;
-
 public final class AssetCreateRequestToEntityMapper {
 
     private AssetCreateRequestToEntityMapper() {
     }
 
     public static AssetEntity map(AssetCreateRequest createRequest) {
-        return AssetEntity.builder()
-                .name(createRequest.getName())
-                .price(createRequest.getPrice())
-                .isDefault(createRequest.getIsDefault())
-                .createdAt(LocalDateTime.now())
-                .createdBy("system")
-                .build();
+        return AssetMapper.INSTANCE.toAssetEntity(createRequest);
     }
 }
