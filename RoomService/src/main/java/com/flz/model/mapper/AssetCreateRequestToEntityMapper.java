7a9891fbd2e86b7263e7e5f85c8574fbea21cd44
Filizkh <filizhelvaci82@gmail.com>
2025-01-28 16:43:44 +0300

    createdAt and CreatedBy added to assetEntity

--- RoomService/src/main/java/com/flz/model/mapper/AssetCreateRequestToEntityMapper.java
+++ RoomService/src/main/java/com/flz/model/mapper/AssetCreateRequestToEntityMapper.java
@@ -3,12 +3,17 @@ package com.flz.model.mapper;
 import com.flz.model.entity.AssetEntity;
 import com.flz.model.request.AssetCreateRequest;
 
+import java.time.LocalDateTime;
+
 public final class AssetCreateRequestToEntityMapper {
 
     private AssetCreateRequestToEntityMapper() {
     }
 
-    public static AssetEntity map(AssetCreateRequest createRequest) {
-        return AssetMapper.INSTANCE.toAssetEntity(createRequest);
+    public static AssetEntity map(AssetCreateRequest assetcreateRequest) {
+        AssetEntity assetEntity = AssetMapper.INSTANCE.toAssetEntity(assetcreateRequest);
+        assetEntity.setCreatedAt(LocalDateTime.now());
+        assetEntity.setCreatedBy("admin");
+        return assetEntity;
     }
 }
