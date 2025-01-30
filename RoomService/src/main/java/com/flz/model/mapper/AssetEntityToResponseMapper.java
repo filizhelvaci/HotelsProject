9561cdccb6650e6ca4d41ee4fdd78bda0f6de5a5
Filizkh <filizhelvaci82@gmail.com>
2025-01-30 14:23:46 +0300

    AssetEntityToResponseMapper interface done.Mapper definition was made within this class.

--- RoomService/src/main/java/com/flz/model/mapper/AssetEntityToResponseMapper.java
+++ RoomService/src/main/java/com/flz/model/mapper/AssetEntityToResponseMapper.java
@@ -2,26 +2,12 @@ package com.flz.model.mapper;
 
 import com.flz.model.entity.AssetEntity;
 import com.flz.model.response.AssetResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-import java.util.ArrayList;
-import java.util.List;
+@Mapper
+public interface AssetEntityToResponseMapper extends BaseMapper<AssetEntity, AssetResponse> {
 
-public final class AssetEntityToResponseMapper {
-
-    private AssetEntityToResponseMapper() {
-    }
-
-    public static AssetResponse map(AssetEntity assetEntity) {
-        return AssetMapper.INSTANCE.toAssetResponse(assetEntity);
-    }
-
-    public static List<AssetResponse> map(List<AssetEntity> assetEntities) {
-        List<AssetResponse> assetResponses = new ArrayList<>();
-        for (AssetEntity assetEntity : assetEntities) {
-            AssetResponse assetResponse = map(assetEntity);
-            assetResponses.add(assetResponse);
-        }
-        return assetResponses;
-    }
+    AssetEntityToResponseMapper INSTANCE = Mappers.getMapper(AssetEntityToResponseMapper.class);
 
 }
