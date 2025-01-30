86521a4250cbc4f53b8ed4094afbb2edbcfb394c
Filizkh <filizhelvaci82@gmail.com>
2025-01-30 20:59:01 +0300

    @Setter added to RoomTypeAvailabilityResponse

--- RoomService/src/main/java/com/flz/model/response/RoomTypeAvailabilityResponse.java
+++ RoomService/src/main/java/com/flz/model/response/RoomTypeAvailabilityResponse.java
@@ -2,10 +2,12 @@ package com.flz.model.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
 @Builder
 public class RoomTypeAvailabilityResponse {
 
