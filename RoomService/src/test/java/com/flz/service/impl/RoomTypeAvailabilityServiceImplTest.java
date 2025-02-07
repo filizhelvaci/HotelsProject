f9726c867bdb39a3ef2749c075d2e59a413d8d27
Filizkh <filizhelvaci82@gmail.com>
2025-02-07 12:09:31 +0300

    RoomTypeAvailabilityServiceImplTest All Room.Status are FULL scenario done. RoomEntity list or RoomTypeEntity list is empty() scenario done

--- RoomService/src/test/java/com/flz/service/impl/RoomTypeAvailabilityServiceImplTest.java
+++ RoomService/src/test/java/com/flz/service/impl/RoomTypeAvailabilityServiceImplTest.java
@@ -18,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -100,4 +101,100 @@ class RoomTypeAvailabilityServiceImplTest {
         Mockito.verify(roomRepository, Mockito.times(1)).findAll();
 
     }
+
+    @Test
+    public void givenTheCustomerWantsToSeeTheRooms_whenAllRoomsStatusAreFull_thenReturnRoomTypeUnavailability() {
+
+        //Given
+        List<AssetEntity> assets = List.of(
+                AssetEntity.builder().id(1L).name("Yatak Seti").price(BigDecimal.valueOf(100)).isDefault(false).build(),
+                AssetEntity.builder().id(2L).name("55 inç LCD TV").price(BigDecimal.valueOf(100)).isDefault(false).build(),
+                AssetEntity.builder().id(3L).name("Wifi").price(BigDecimal.valueOf(0)).isDefault(false).build(),
+                AssetEntity.builder().id(4L).name("Çay/kahve makinesi").price(BigDecimal.valueOf(100)).isDefault(false).build()
+        );
+        List<RoomTypeEntity> roomTypeEntities = List.of(RoomTypeEntity.builder()
+                        .id(1L)
+                        .name("Standart Oda")
+                        .price(BigDecimal.valueOf(2000))
+                        .size(30)
+                        .personCount(2)
+                        .description("Bahçe manzaralı bu oda standart şekilde tasarlanmıştır ve bir yatak odası bulunmaktadır. " +
+                                "Bir kanepe/koltuk bulunur ve tuvaleti olan bir banyo sunmaktadır. " +
+                                "Ayrıca WiFi, 55 inç LCD TV, MP3 çalar/radyo/çalar saat, çay/kahve yapma olanaklarını " +
+                                "ve WiFi erişimini kapsamaktadır.")
+                        .assets(assets)
+                        .build(),
+                RoomTypeEntity.builder()
+                        .id(2L)
+                        .name("Lux Oda")
+                        .price(BigDecimal.valueOf(4000))
+                        .size(50)
+                        .personCount(3)
+                        .description("Bahçe manzarası, balkon, küvet, duş ve ayrı tuvalet, 55-inç TV, WiFi, " +
+                                "50 metrekarelik bu konforlu ve ferah bahçe manzaralı odada bir king yatak, balkon, " +
+                                "kanepe veya koltuk, küvetli banyo, duş ve ayrı tuvalet bulunmaktadır. " +
+                                "55-inç LCD televizyonda bir programın veya MP3 çalar radyo-çalar saatte müziğin keyfini çıkarın." +
+                                "Diğer hizmetler arasında WiFi ve çay/kahve yapma olanakları yer almaktadır.")
+                        .assets(assets)
+                        .build()
+        );
+
+        List<RoomEntity> roomEntities = List.of(RoomEntity.builder()
+                        .id(1L)
+                        .number(101)
+                        .floor(1)
+                        .status(RoomStatus.FULL)
+                        .type(roomTypeEntities.get(1))
+                        .build(),
+                RoomEntity.builder()
+                        .id(2L)
+                        .number(102)
+                        .floor(1)
+                        .status(RoomStatus.FULL)
+                        .type(roomTypeEntities.get(0))
+                        .build()
+        );
+
+        //When
+        Mockito.when(roomRepository.findAll()).thenReturn(roomEntities);
+        Mockito.when(roomTypeRepository.findAll()).thenReturn(roomTypeEntities);
+
+        List<RoomTypeAvailabilityResponse> roomTypeAvailabilityResponses = roomTypeAvailabilityServiceImpl.findRoomTypesAvailability();
+
+        //Then
+        Assertions.assertNotNull(roomTypeAvailabilityResponses);
+        Assertions.assertFalse(roomTypeAvailabilityResponses.get(0).getIsAvailability());
+        Mockito.verify(roomTypeRepository, Mockito.times(1)).findAll();
+        Mockito.verify(roomRepository, Mockito.times(1)).findAll();
+
+    }
+
+    @Test
+    public void whenTheCustomerWantsToSeeTheRoomsIfRoomTypeEntitiesIsEmpty_thenReturnEmptyList() {
+
+        //When
+        Mockito.when(roomTypeRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<RoomTypeAvailabilityResponse> roomTypeAvailabilityResponses = roomTypeAvailabilityServiceImpl.findRoomTypesAvailability();
+
+        //Then
+        Assertions.assertNotNull(roomTypeAvailabilityResponses);
+        Assertions.assertTrue(roomTypeAvailabilityResponses.isEmpty());
+
+    }
+
+    @Test
+    public void whenTheCustomerWantsToSeeTheRoomsIfRoomEntitiesIsEmpty_thenReturnEmptyList() {
+
+        //When
+        Mockito.when(roomRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<RoomTypeAvailabilityResponse> roomTypeAvailabilityResponses = roomTypeAvailabilityServiceImpl.findRoomTypesAvailability();
+
+        //Then
+        Assertions.assertNotNull(roomTypeAvailabilityResponses);
+        Assertions.assertTrue(roomTypeAvailabilityResponses.isEmpty());
+
+    }
+
 }
