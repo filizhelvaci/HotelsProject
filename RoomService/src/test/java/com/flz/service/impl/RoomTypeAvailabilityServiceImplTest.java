c8a0bc4c04f88eb3173fb1c7ca53159b4b9f819c
Filizkh <filizhelvaci82@gmail.com>
2025-02-10 11:09:13 +0300

    In RoomTypeAvailabilityServiceImplTest frequently used objects are methodized

--- RoomService/src/test/java/com/flz/service/impl/RoomTypeAvailabilityServiceImplTest.java
+++ RoomService/src/test/java/com/flz/service/impl/RoomTypeAvailabilityServiceImplTest.java
@@ -29,59 +29,16 @@ class RoomTypeAvailabilityServiceImplTest extends BaseTest {
     @InjectMocks
     RoomTypeAvailabilityServiceImpl roomTypeAvailabilityServiceImpl;
 
-
+    /**
+     * {@link RoomTypeAvailabilityServiceImpl#findRoomTypesAvailability()}
+     */
     @Test
     public void whenTheCustomerWantsToSeeTheRooms_thenListRoomTypesAndAvailability() {
 
         //Given
-        List<AssetEntity> assets = List.of(
-                AssetEntity.builder().id(1L).name("Yatak Seti").price(BigDecimal.valueOf(100)).isDefault(false).build(),
-                AssetEntity.builder().id(2L).name("55 inç LCD TV").price(BigDecimal.valueOf(100)).isDefault(false).build(),
-                AssetEntity.builder().id(3L).name("Wifi").price(BigDecimal.valueOf(0)).isDefault(false).build(),
-                AssetEntity.builder().id(4L).name("Çay/kahve makinesi").price(BigDecimal.valueOf(100)).isDefault(false).build()
-        );
-        List<RoomTypeEntity> roomTypeEntities = List.of(RoomTypeEntity.builder()
-                        .id(1L)
-                        .name("Standart Oda")
-                        .price(BigDecimal.valueOf(2000))
-                        .size(30)
-                        .personCount(2)
-                        .description("Bahçe manzaralı bu oda standart şekilde tasarlanmıştır ve bir yatak odası bulunmaktadır. " +
-                                "Bir kanepe/koltuk bulunur ve tuvaleti olan bir banyo sunmaktadır. " +
-                                "Ayrıca WiFi, 55 inç LCD TV, MP3 çalar/radyo/çalar saat, çay/kahve yapma olanaklarını " +
-                                "ve WiFi erişimini kapsamaktadır.")
-                        .assets(assets)
-                        .build(),
-                RoomTypeEntity.builder()
-                        .id(2L)
-                        .name("Lux Oda")
-                        .price(BigDecimal.valueOf(4000))
-                        .size(50)
-                        .personCount(3)
-                        .description("Bahçe manzarası, balkon, küvet, duş ve ayrı tuvalet, 55-inç TV, WiFi, " +
-                                "50 metrekarelik bu konforlu ve ferah bahçe manzaralı odada bir king yatak, balkon, " +
-                                "kanepe veya koltuk, küvetli banyo, duş ve ayrı tuvalet bulunmaktadır. " +
-                                "55-inç LCD televizyonda bir programın veya MP3 çalar radyo-çalar saatte müziğin keyfini çıkarın." +
-                                "Diğer hizmetler arasında WiFi ve çay/kahve yapma olanakları yer almaktadır.")
-                        .assets(assets)
-                        .build()
-        );
-
-        List<RoomEntity> roomEntities = List.of(RoomEntity.builder()
-                        .id(1L)
-                        .number(101)
-                        .floor(1)
-                        .status(RoomStatus.EMPTY)
-                        .type(roomTypeEntities.get(1))
-                        .build(),
-                RoomEntity.builder()
-                        .id(2L)
-                        .number(102)
-                        .floor(1)
-                        .status(RoomStatus.EMPTY)
-                        .type(roomTypeEntities.get(0))
-                        .build()
-        );
+        List<AssetEntity> assets = getAssets();
+        List<RoomTypeEntity> roomTypeEntities = getRoomTypes(assets);
+        List<RoomEntity> roomEntities = getRooms(roomTypeEntities);
 
         //When
         Mockito.when(roomRepository.findAll()).thenReturn(roomEntities);
@@ -97,58 +54,15 @@ class RoomTypeAvailabilityServiceImplTest extends BaseTest {
 
     }
 
+
     @Test
     public void givenTheCustomerWantsToSeeTheRooms_whenAllRoomsStatusAreFull_thenReturnRoomTypeUnavailability() {
 
         //Given
-        List<AssetEntity> assets = List.of(
-                AssetEntity.builder().id(1L).name("Yatak Seti").price(BigDecimal.valueOf(100)).isDefault(false).build(),
-                AssetEntity.builder().id(2L).name("55 inç LCD TV").price(BigDecimal.valueOf(100)).isDefault(false).build(),
-                AssetEntity.builder().id(3L).name("Wifi").price(BigDecimal.valueOf(0)).isDefault(false).build(),
-                AssetEntity.builder().id(4L).name("Çay/kahve makinesi").price(BigDecimal.valueOf(100)).isDefault(false).build()
-        );
-        List<RoomTypeEntity> roomTypeEntities = List.of(RoomTypeEntity.builder()
-                        .id(1L)
-                        .name("Standart Oda")
-                        .price(BigDecimal.valueOf(2000))
-                        .size(30)
-                        .personCount(2)
-                        .description("Bahçe manzaralı bu oda standart şekilde tasarlanmıştır ve bir yatak odası bulunmaktadır. " +
-                                "Bir kanepe/koltuk bulunur ve tuvaleti olan bir banyo sunmaktadır. " +
-                                "Ayrıca WiFi, 55 inç LCD TV, MP3 çalar/radyo/çalar saat, çay/kahve yapma olanaklarını " +
-                                "ve WiFi erişimini kapsamaktadır.")
-                        .assets(assets)
-                        .build(),
-                RoomTypeEntity.builder()
-                        .id(2L)
-                        .name("Lux Oda")
-                        .price(BigDecimal.valueOf(4000))
-                        .size(50)
-                        .personCount(3)
-                        .description("Bahçe manzarası, balkon, küvet, duş ve ayrı tuvalet, 55-inç TV, WiFi, " +
-                                "50 metrekarelik bu konforlu ve ferah bahçe manzaralı odada bir king yatak, balkon, " +
-                                "kanepe veya koltuk, küvetli banyo, duş ve ayrı tuvalet bulunmaktadır. " +
-                                "55-inç LCD televizyonda bir programın veya MP3 çalar radyo-çalar saatte müziğin keyfini çıkarın." +
-                                "Diğer hizmetler arasında WiFi ve çay/kahve yapma olanakları yer almaktadır.")
-                        .assets(assets)
-                        .build()
-        );
+        List<AssetEntity> assets = getAssets();
+        List<RoomTypeEntity> roomTypeEntities = getRoomTypes(assets);
 
-        List<RoomEntity> roomEntities = List.of(RoomEntity.builder()
-                        .id(1L)
-                        .number(101)
-                        .floor(1)
-                        .status(RoomStatus.FULL)
-                        .type(roomTypeEntities.get(1))
-                        .build(),
-                RoomEntity.builder()
-                        .id(2L)
-                        .number(102)
-                        .floor(1)
-                        .status(RoomStatus.FULL)
-                        .type(roomTypeEntities.get(0))
-                        .build()
-        );
+        List<RoomEntity> roomEntities = getRooms(roomTypeEntities);
 
         //When
         Mockito.when(roomRepository.findAll()).thenReturn(roomEntities);
@@ -191,5 +105,66 @@ class RoomTypeAvailabilityServiceImplTest extends BaseTest {
         Assertions.assertTrue(roomTypeAvailabilityResponses.isEmpty());
 
     }
+
+
+    /**
+     * @return
+     */
+    private static List<RoomEntity> getRooms(List<RoomTypeEntity> roomTypeEntities) {
+        return List.of(RoomEntity.builder()
+                        .id(1L)
+                        .number(101)
+                        .floor(1)
+                        .status(RoomStatus.EMPTY)
+                        .type(roomTypeEntities.get(1))
+                        .build(),
+                RoomEntity.builder()
+                        .id(2L)
+                        .number(102)
+                        .floor(1)
+                        .status(RoomStatus.EMPTY)
+                        .type(roomTypeEntities.get(0))
+                        .build()
+        );
+    }
+
+    private static List<RoomTypeEntity> getRoomTypes(List<AssetEntity> assets) {
+        return List.of(RoomTypeEntity.builder()
+                        .id(1L)
+                        .name("Standart Oda")
+                        .price(BigDecimal.valueOf(2000))
+                        .size(30)
+                        .personCount(2)
+                        .description("Bahçe manzaralı bu oda standart şekilde tasarlanmıştır ve bir yatak odası bulunmaktadır. " +
+                                "Bir kanepe/koltuk bulunur ve tuvaleti olan bir banyo sunmaktadır. " +
+                                "Ayrıca WiFi, 55 inç LCD TV, MP3 çalar/radyo/çalar saat, çay/kahve yapma olanaklarını " +
+                                "ve WiFi erişimini kapsamaktadır.")
+                        .assets(assets)
+                        .build(),
+                RoomTypeEntity.builder()
+                        .id(2L)
+                        .name("Lux Oda")
+                        .price(BigDecimal.valueOf(4000))
+                        .size(50)
+                        .personCount(3)
+                        .description("Bahçe manzarası, balkon, küvet, duş ve ayrı tuvalet, 55-inç TV, WiFi, " +
+                                "50 metrekarelik bu konforlu ve ferah bahçe manzaralı odada bir king yatak, balkon, " +
+                                "kanepe veya koltuk, küvetli banyo, duş ve ayrı tuvalet bulunmaktadır. " +
+                                "55-inç LCD televizyonda bir programın veya MP3 çalar radyo-çalar saatte müziğin keyfini çıkarın." +
+                                "Diğer hizmetler arasında WiFi ve çay/kahve yapma olanakları yer almaktadır.")
+                        .assets(assets)
+                        .build()
+        );
+    }
+
+    private static List<AssetEntity> getAssets() {
+        return List.of(
+                AssetEntity.builder().id(1L).name("Yatak Seti").price(BigDecimal.valueOf(100)).isDefault(false).build(),
+                AssetEntity.builder().id(2L).name("55 inç LCD TV").price(BigDecimal.valueOf(100)).isDefault(false).build(),
+                AssetEntity.builder().id(3L).name("Wifi").price(BigDecimal.valueOf(0)).isDefault(false).build(),
+                AssetEntity.builder().id(4L).name("Çay/kahve makinesi").price(BigDecimal.valueOf(100)).isDefault(false).build()
+        );
+    }
+
 
 }
