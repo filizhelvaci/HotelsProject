e04e6123628a7a4722037f8b4ed386a53f71bb6b
Filizkh <filizhelvaci82@gmail.com>
2025-02-13 15:12:15 +0300

    In Asset Controller Tests edited

--- RoomService/src/test/java/com/flz/controller/AssetControllerTest.java
+++ RoomService/src/test/java/com/flz/controller/AssetControllerTest.java
@@ -46,235 +46,6 @@ class AssetControllerTest extends BaseTest {
     private static final String BASE_PATH = "/api/v1";
 
     /**
-     * Create()
-     * {@link AssetController#create(AssetCreateRequest)}
-     */
-    @Test
-    public void givenValidAssetCreateRequest_whenAssetCreated_thenSuccessResponse() throws Exception {
-
-        //Given
-        AssetCreateRequest mockAssetCreateRequest = new AssetCreateRequest();
-        mockAssetCreateRequest.setName("test");
-        mockAssetCreateRequest.setPrice(BigDecimal.valueOf(250));
-        mockAssetCreateRequest.setIsDefault(true);
-
-        //When
-        Mockito.doNothing().when(assetService).create(Mockito.any(AssetCreateRequest.class));
-
-        //Then
-        mockMvc.perform(post(BASE_PATH + "/asset")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(mockAssetCreateRequest)))
-                .andExpect(status().isOk());
-
-        //Verify
-        Mockito.verify(assetService, Mockito.times(1))
-                .create(Mockito.any(AssetCreateRequest.class));
-
-    }
-
-    @Test
-    public void givenAssetCreateRequestWithMissingFields_whenCreateAsset_thenBadRequestResponse() throws Exception {
-
-        //Given
-        AssetCreateRequest invalidRequest = new AssetCreateRequest();
-        invalidRequest.setName("");
-
-        //When
-        mockMvc.perform(post(BASE_PATH + "/asset")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(invalidRequest)))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    public void givenAssetServiceThrowsException_whenCreateAsset_thenInternalServerErrorResponse() throws Exception {
-
-        //Given
-        AssetCreateRequest mockAssetCreateRequest = new AssetCreateRequest();
-        mockAssetCreateRequest.setName("test");
-        mockAssetCreateRequest.setPrice(BigDecimal.valueOf(250));
-        mockAssetCreateRequest.setIsDefault(true);
-
-        //When
-        Mockito.doThrow(new RuntimeException("Unexpected Error")).when(assetService)
-                .create(Mockito.any(AssetCreateRequest.class));
-
-        //Then
-        mockMvc.perform(post(BASE_PATH + "/asset")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(mockAssetCreateRequest)))
-                .andExpect(status().isInternalServerError());
-    }
-
-    /**
-     * findById()
-     * {@link AssetController#findById(Long)}
-     */
-    @Test
-    public void givenValidId_whenFindAssetById_thenReturnAssetResponse() throws Exception {
-
-        //Given
-        Long mockId = 10L;
-
-        //When
-        AssetResponse mockAssetResponse = AssetResponse.builder()
-                .id(10L)
-                .name("test")
-                .price(BigDecimal.valueOf(250))
-                .isDefault(true)
-                .build();
-
-        Mockito.when(assetService.findById(mockId)).thenReturn(mockAssetResponse);
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/asset/{id}", mockId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.response.id").value(mockId))
-                .andExpect(jsonPath("$.response.name").value("test"))
-                .andExpect(jsonPath("$.response.price").value(250))
-                .andExpect(jsonPath("$.response.isDefault").value(true));
-
-        //Verify
-        Mockito.verify(assetService, Mockito.times(1)).findById(mockId);
-    }
-
-    @Test
-    public void givenNonAssetId_whenNotFoundById_thenReturnNotFoundException() throws Exception {
-
-        //Given
-        Long nonAssetId = 999L;
-
-        //When
-        Mockito.when(assetService.findById(nonAssetId))
-                .thenThrow(new AssetNotFoundException(nonAssetId));
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/asset/{id}", nonAssetId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-
-        //Verify
-        Mockito.verify(assetService, Mockito.times(1))
-                .findById(nonAssetId);
-    }
-
-    @Test
-    public void givenInvalidAssetId_whenNotFoundById_thenReturnBadRequest() throws Exception {
-
-        //Given
-        String invalidAssetId = "ukhd-3521";
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/asset/{id}", invalidAssetId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-
-        //Verify
-        Mockito.verify(assetService, Mockito.never())
-                .findById(Mockito.any());
-    }
-
-    @Test
-    public void givenServerError_whenFindById_thenReturnInternalServerError() throws Exception {
-
-        //Given
-        Long mockId = 1L;
-
-        //When
-        Mockito.when(assetService.findById(mockId))
-                .thenThrow(new RuntimeException("Unexpected error"));
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/asset/{id}", mockId)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isInternalServerError());
-
-    }
-
-    /**
-     * findSummaryAll()
-     * {@link AssetService#findSummaryAll()}
-     */
-    @Test
-    public void whenCallAllSummaryAsset_thenReturnAssetsSummaryResponse() throws Exception {
-
-        //Given
-        List<AssetsSummaryResponse> mockAssetsSummaryResponse =
-                List.of(AssetsSummaryResponse.builder()
-                                .id(11L)
-                                .name("test1")
-                                .build(),
-                        AssetsSummaryResponse.builder()
-                                .id(12L)
-                                .name("test2")
-                                .build(),
-                        AssetsSummaryResponse.builder()
-                                .id(13L)
-                                .name("test3")
-                                .build()
-                );
-
-        //When
-        Mockito.when(assetService.findSummaryAll()).thenReturn(mockAssetsSummaryResponse);
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/assets/summary")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.isSuccess").value(true))
-                .andExpect(jsonPath("$.response").isArray())
-                .andDo(print());
-
-        //Verify
-        Mockito.verify(assetService, Mockito.times(1)).findSummaryAll();
-
-    }
-
-    @Test
-    public void givenNonAssets_whenNotFoundSummaryAll_thenReturnEmptyList() throws Exception {
-
-        //When
-        List<AssetsSummaryResponse> emptyList = Collections.emptyList();
-
-        Mockito.when(assetService.findSummaryAll())
-                .thenReturn(emptyList);
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/assets/summary")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.isSuccess").value(true))
-                .andExpect(jsonPath("$.response").isArray())
-                .andExpect(jsonPath("$.response").isEmpty());
-
-        //Verify
-        Mockito.verify(assetService, Mockito.times(1))
-                .findSummaryAll();
-    }
-
-    @Test
-    public void whenFindSummaryAllIsCalledAndTheServiceFails_thenReturnInternalServerError() throws Exception {
-
-        //When
-        Mockito.when(assetService.findSummaryAll())
-                .thenThrow(new RuntimeException("An unexpected error occurred"));
-
-        //Then
-        mockMvc.perform(get(BASE_PATH + "/assets/summary")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.isSuccess").value(false));
-
-        //Verify
-        Mockito.verify(assetService, Mockito.times(1))
-                .findSummaryAll();
-
-    }
-
-    /**
      * findAll()
      * {@link AssetController#findAll(String, BigDecimal, BigDecimal, Boolean, int, int, String, Sort.Direction)}
      */
@@ -400,6 +171,235 @@ class AssetControllerTest extends BaseTest {
     }
 
     /**
+     * findSummaryAll()
+     * {@link AssetService#findSummaryAll()}
+     */
+    @Test
+    public void whenCallAllSummaryAsset_thenReturnAssetsSummaryResponse() throws Exception {
+
+        //Given
+        List<AssetsSummaryResponse> mockAssetsSummaryResponse =
+                List.of(AssetsSummaryResponse.builder()
+                                .id(11L)
+                                .name("test1")
+                                .build(),
+                        AssetsSummaryResponse.builder()
+                                .id(12L)
+                                .name("test2")
+                                .build(),
+                        AssetsSummaryResponse.builder()
+                                .id(13L)
+                                .name("test3")
+                                .build()
+                );
+
+        //When
+        Mockito.when(assetService.findSummaryAll()).thenReturn(mockAssetsSummaryResponse);
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/assets/summary")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.response").isArray())
+                .andDo(print());
+
+        //Verify
+        Mockito.verify(assetService, Mockito.times(1)).findSummaryAll();
+
+    }
+
+    @Test
+    public void givenNonAssets_whenNotFoundSummaryAll_thenReturnEmptyList() throws Exception {
+
+        //When
+        List<AssetsSummaryResponse> emptyList = Collections.emptyList();
+
+        Mockito.when(assetService.findSummaryAll())
+                .thenReturn(emptyList);
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/assets/summary")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.response").isArray())
+                .andExpect(jsonPath("$.response").isEmpty());
+
+        //Verify
+        Mockito.verify(assetService, Mockito.times(1))
+                .findSummaryAll();
+    }
+
+    @Test
+    public void whenFindSummaryAllIsCalledAndTheServiceFails_thenReturnInternalServerError() throws Exception {
+
+        //When
+        Mockito.when(assetService.findSummaryAll())
+                .thenThrow(new RuntimeException("An unexpected error occurred"));
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/assets/summary")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.isSuccess").value(false));
+
+        //Verify
+        Mockito.verify(assetService, Mockito.times(1))
+                .findSummaryAll();
+
+    }
+
+    /**
+     * findById()
+     * {@link AssetController#findById(Long)}
+     */
+    @Test
+    public void givenValidId_whenFindAssetById_thenReturnAssetResponse() throws Exception {
+
+        //Given
+        Long mockId = 10L;
+
+        //When
+        AssetResponse mockAssetResponse = AssetResponse.builder()
+                .id(10L)
+                .name("test")
+                .price(BigDecimal.valueOf(250))
+                .isDefault(true)
+                .build();
+
+        Mockito.when(assetService.findById(mockId)).thenReturn(mockAssetResponse);
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/asset/{id}", mockId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.id").value(mockId))
+                .andExpect(jsonPath("$.response.name").value("test"))
+                .andExpect(jsonPath("$.response.price").value(250))
+                .andExpect(jsonPath("$.response.isDefault").value(true));
+
+        //Verify
+        Mockito.verify(assetService, Mockito.times(1)).findById(mockId);
+    }
+
+    @Test
+    public void givenNonAssetId_whenNotFoundById_thenReturnNotFoundException() throws Exception {
+
+        //Given
+        Long nonAssetId = 999L;
+
+        //When
+        Mockito.when(assetService.findById(nonAssetId))
+                .thenThrow(new AssetNotFoundException(nonAssetId));
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/asset/{id}", nonAssetId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        //Verify
+        Mockito.verify(assetService, Mockito.times(1))
+                .findById(nonAssetId);
+    }
+
+    @Test
+    public void givenInvalidAssetId_whenNotFoundById_thenReturnBadRequest() throws Exception {
+
+        //Given
+        String invalidAssetId = "ukhd-3521";
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/asset/{id}", invalidAssetId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        //Verify
+        Mockito.verify(assetService, Mockito.never())
+                .findById(Mockito.any());
+    }
+
+    @Test
+    public void givenServerError_whenFindById_thenReturnInternalServerError() throws Exception {
+
+        //Given
+        Long mockId = 1L;
+
+        //When
+        Mockito.when(assetService.findById(mockId))
+                .thenThrow(new RuntimeException("Unexpected error"));
+
+        //Then
+        mockMvc.perform(get(BASE_PATH + "/asset/{id}", mockId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
+
+    }
+
+    /**
+     * Create()
+     * {@link AssetController#create(AssetCreateRequest)}
+     */
+    @Test
+    public void givenValidAssetCreateRequest_whenAssetCreated_thenSuccessResponse() throws Exception {
+
+        //Given
+        AssetCreateRequest mockAssetCreateRequest = new AssetCreateRequest();
+        mockAssetCreateRequest.setName("test");
+        mockAssetCreateRequest.setPrice(BigDecimal.valueOf(250));
+        mockAssetCreateRequest.setIsDefault(true);
+
+        //When
+        Mockito.doNothing().when(assetService).create(Mockito.any(AssetCreateRequest.class));
+
+        //Then
+        mockMvc.perform(post(BASE_PATH + "/asset")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(mockAssetCreateRequest)))
+                .andExpect(status().isOk());
+
+        //Verify
+        Mockito.verify(assetService, Mockito.times(1))
+                .create(Mockito.any(AssetCreateRequest.class));
+
+    }
+
+    @Test
+    public void givenAssetCreateRequestWithMissingFields_whenCreateAsset_thenBadRequestResponse() throws Exception {
+
+        //Given
+        AssetCreateRequest invalidRequest = new AssetCreateRequest();
+        invalidRequest.setName("");
+
+        //When
+        mockMvc.perform(post(BASE_PATH + "/asset")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(invalidRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenAssetServiceThrowsException_whenCreateAsset_thenInternalServerErrorResponse() throws Exception {
+
+        //Given
+        AssetCreateRequest mockAssetCreateRequest = new AssetCreateRequest();
+        mockAssetCreateRequest.setName("test");
+        mockAssetCreateRequest.setPrice(BigDecimal.valueOf(250));
+        mockAssetCreateRequest.setIsDefault(true);
+
+        //When
+        Mockito.doThrow(new RuntimeException("Unexpected Error")).when(assetService)
+                .create(Mockito.any(AssetCreateRequest.class));
+
+        //Then
+        mockMvc.perform(post(BASE_PATH + "/asset")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(mockAssetCreateRequest)))
+                .andExpect(status().isInternalServerError());
+    }
+
+    /**
      * Update()
      * {@link AssetController#update(Long, AssetUpdateRequest)}
      */
@@ -504,7 +504,7 @@ class AssetControllerTest extends BaseTest {
     }
 
     /**
-     *  # Methodized Objects
+     * # Methodized Objects
      */
     private static List<AssetsResponse> getAssetsResponse() {
         return List.of(AssetsResponse.builder()
