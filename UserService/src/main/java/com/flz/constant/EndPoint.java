e921a37730413955ad240b0afe8f9c5e74541654
Filizkh <filizhelvaci82@gmail.com>
2025-02-16 11:38:05 +0300

    For UserService done cleaning

--- UserService/src/main/java/com/flz/constant/EndPoint.java
+++
@@ -1,8 +0,0 @@
-package com.flz.constant;
-
-public class EndPoint {
-
-    public static final  String REGISTER = "/register";
-    public static final  String LOGIN = "/login";
-
-}
--- UserService/src/main/java/com/flz/repository/EmployeesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flz.repository;
-
-import com.flz.model.Employees;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeesRepository extends JpaRepository<Employees, Long> {
-}
