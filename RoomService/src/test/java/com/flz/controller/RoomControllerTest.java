2b4ab19a429f914702309ca665008950ba183c21
Filizkh <filizhelvaci82@gmail.com>
2025-02-22 01:08:42 +0300

    Tested return BadRequest when RoomUpdateRequest was called with invalid Number

--- RoomService/src/test/java/com/flz/controller/RoomControllerTest.java
+++ RoomService/src/test/java/com/flz/controller/RoomControllerTest.java
@@ -791,6 +791,45 @@ class RoomControllerTest extends BaseTest {
     }
 
     @ParameterizedTest
+    @NullSource
+    @ValueSource(ints = {
+            -1,
+            10001
+    })
+    void givenInvalidNumber_whenRoomUpdateRequest_thenBadRequestResponse(Integer invalidRequest) throws Exception {
+
+        //Given
+        Long mockId = 10L;
+
+        RoomUpdateRequest mockRoomUpdateRequest = new RoomUpdateRequest();
+        mockRoomUpdateRequest.setNumber(invalidRequest);
+        mockRoomUpdateRequest.setFloor(3);
+        mockRoomUpdateRequest.setRoomTypeId(2L);
+        mockRoomUpdateRequest.setStatus(RoomStatus.FULL);
+
+        //When
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders
+                .put(BASE_PATH + "/room/{id}", mockId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(mockRoomUpdateRequest));
+
+        //Then
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status()
+                        .isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess")
+                        .value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
+                        .isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.field")
+                        .value("roomUpdateRequest"));
+
+        // Verify
+        Mockito.verify(roomService, Mockito.never()).update(Mockito.anyLong(), Mockito.any(RoomUpdateRequest.class));
+    }
+
+    @ParameterizedTest
     @MethodSource("invalidRoomUpdateRequests")
     void givenInvalidRoomUpdateRequests_whenUpdateRoom_thenBadRequestResponse(RoomUpdateRequest invalidRequest) throws Exception {
 
