5f03e39bed4eba734e5cb59aa91019272a1dea03
Filizkh <filizhelvaci82@gmail.com>
2025-02-23 14:28:30 +0300

    It should return Bad Request When RoomType-Update called with RoomTypeUpdateRequest Invalid and Null Description - tested

--- RoomService/src/test/java/com/flz/controller/RoomTypeControllerTest.java
+++ RoomService/src/test/java/com/flz/controller/RoomTypeControllerTest.java
@@ -1242,7 +1242,7 @@ class RoomTypeControllerTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {
             "-1",
-            "10001",
+            "1001",
     })
     void givenInvalidSize_whenRoomTypeUpdateRequest_thenBadRequestResponse(String invalidRequest) throws Exception {
 
@@ -1290,6 +1290,51 @@ class RoomTypeControllerTest extends BaseTest {
         mockRoomTypeUpdateRequest.setPersonCount(3);
         mockRoomTypeUpdateRequest.setSize(null);
         mockRoomTypeUpdateRequest.setDescription("this is a description");
+        mockRoomTypeUpdateRequest.setAssetIds(List.of(1L, 3L, 5L));
+
+        //When
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders
+                .put(BASE_PATH + "/room-type/{id}", mockId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(mockRoomTypeUpdateRequest));
+
+        //Then
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status()
+                        .isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess")
+                        .value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
+                        .isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.field")
+                        .value("roomTypeUpdateRequest"));
+
+        // Verify
+        Mockito.verify(roomTypeService, Mockito.never())
+                .update(Mockito.any(), Mockito.any(RoomTypeUpdateRequest.class));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {
+            "",
+            "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. \"What's happened to me?\" he thought.\n" +
+                    "\n" +
+                    "It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.\n" +
+                    "\n" +
+                    "It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards",
+    })
+    void givenInvalidDescription_whenRoomTypeUpdateRequest_thenBadRequestResponse(String invalidRequest) throws Exception {
+
+        //Given
+        Long mockId = 10L;
+
+        RoomTypeUpdateRequest mockRoomTypeUpdateRequest = new RoomTypeUpdateRequest();
+        mockRoomTypeUpdateRequest.setName("Standart Room");
+        mockRoomTypeUpdateRequest.setPersonCount(3);
+        mockRoomTypeUpdateRequest.setSize(50);
+        mockRoomTypeUpdateRequest.setDescription(invalidRequest);
         mockRoomTypeUpdateRequest.setAssetIds(List.of(1L, 3L, 5L));
 
         //When
