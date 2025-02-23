422ff01c09b005d6395ab9ab1b30ca4f404b7e1c
Filizkh <filizhelvaci82@gmail.com>
2025-02-23 13:35:19 +0300

    It should return Bad Request When RoomType-Create called with RoomTypeCreateRequest Invalid and Null AssetIdList - tested

--- RoomService/src/test/java/com/flz/controller/RoomTypeControllerTest.java
+++ RoomService/src/test/java/com/flz/controller/RoomTypeControllerTest.java
@@ -31,6 +31,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
@@ -940,9 +941,53 @@ class RoomTypeControllerTest extends BaseTest {
         mockRoomTypeCreateRequest.setName("Delux Room");
         mockRoomTypeCreateRequest.setPrice(BigDecimal.valueOf(2500));
         mockRoomTypeCreateRequest.setPersonCount(3);
-        mockRoomTypeCreateRequest.setSize(5);
+        mockRoomTypeCreateRequest.setSize(50);
         mockRoomTypeCreateRequest.setDescription(invalidRequest);
         mockRoomTypeCreateRequest.setAssetIds(List.of(1L, 3L, 5L));
+
+        //When
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders
+                .post(BASE_PATH + "/room-type")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(mockRoomTypeCreateRequest));
+
+        //Then
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status()
+                        .isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.isSuccess")
+                        .value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
+                        .isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.field")
+                        .value("roomTypeCreateRequest"));
+
+        // Verify
+        Mockito.verify(roomTypeService, Mockito.never()).create(Mockito.any());
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {
+            ""
+    })
+    void givenEmptyAssetsIdList_whenRoomTypeCreateRequest_thenBadRequestResponse(String invalidRequest) throws Exception {
+
+        //Given
+        RoomTypeCreateRequest mockRoomTypeCreateRequest = new RoomTypeCreateRequest();
+
+        mockRoomTypeCreateRequest.setName("Delux Room");
+        mockRoomTypeCreateRequest.setPrice(BigDecimal.valueOf(2500));
+        mockRoomTypeCreateRequest.setPersonCount(3);
+        mockRoomTypeCreateRequest.setSize(50);
+        mockRoomTypeCreateRequest.setDescription("this is a description");
+
+        List<Long> assetIds = null;
+        if (invalidRequest != null) {
+            assetIds = new ArrayList<>();
+        }
+        mockRoomTypeCreateRequest.setAssetIds(assetIds);
 
         //When
         MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders
