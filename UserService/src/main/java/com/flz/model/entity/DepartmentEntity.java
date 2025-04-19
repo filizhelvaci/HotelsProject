a0906a22b069cbc79d54e9094ad0fbc4278be040
Filizkh <filizhelvaci82@gmail.com>
2025-04-19 08:03:04 +0300

    DepartmentEntity created for User Service

---
+++ UserService/src/main/java/com/flz/model/entity/DepartmentEntity.java
@@ -0,0 +1,39 @@
+package com.flz.model.entity;
+
+import com.flz.model.enums.DepartmentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ru_department")
+public class DepartmentEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private DepartmentStatus status;
+
+}
