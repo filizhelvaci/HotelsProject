332d19a51e51c91c4957f6cc5a9463f771887e28
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-11 10:19:09 +0300

    update() added

--- UserService/src/main/java/com/flz/service/DepartmentCreateService.java
+++ UserService/src/main/java/com/flz/service/DepartmentCreateService.java
@@ -1,8 +1,12 @@
 package com.flz.service;
 
 import com.flz.model.request.DepartmentCreateRequest;
+import com.flz.model.request.DepartmentUpdateRequest;
 
 public interface DepartmentCreateService {
 
     void create(DepartmentCreateRequest departmentCreateRequest);
+
+    void update(Long id, DepartmentUpdateRequest departmentUpdateRequest);
+
 }
