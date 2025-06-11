c232c98785f66d7081470967b9f4659cd74dcea7
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-11 10:21:08 +0300

    findAll() and existsByName() added.

--- UserService/src/main/java/com/flz/port/DepartmentReadPort.java
+++ UserService/src/main/java/com/flz/port/DepartmentReadPort.java
@@ -2,9 +2,15 @@ package com.flz.port;
 
 import com.flz.model.Department;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentReadPort {
 
+    List<Department> findAll(Integer page, Integer pageSize);
+
     Optional<Department> findById(Long id);
+
+    boolean existsByName(String name);
+
 }
