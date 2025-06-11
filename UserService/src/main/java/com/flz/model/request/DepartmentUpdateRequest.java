ff128d785b1bbd3bcb93c9e292b499ea7daf33c7
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-11 10:27:41 +0300

    DepartmentUpdateRequest created

---
+++ UserService/src/main/java/com/flz/model/request/DepartmentUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.flz.model.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentUpdateRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String name;
+
+}
