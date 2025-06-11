f145c1a82fb2f814c7daf9c97c43ad6db088c813
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-11 10:19:52 +0300

    update() added and create() updated

--- UserService/src/main/java/com/flz/service/impl/DepartmentCreateServiceImpl.java
+++ UserService/src/main/java/com/flz/service/impl/DepartmentCreateServiceImpl.java
@@ -1,8 +1,12 @@
 package com.flz.service.impl;
 
+import com.flz.exception.DepartmentAlreadyExistsException;
+import com.flz.exception.DepartmentNotFoundException;
 import com.flz.model.Department;
 import com.flz.model.mapper.DepartmentCreateRequestToDomainMapper;
 import com.flz.model.request.DepartmentCreateRequest;
+import com.flz.model.request.DepartmentUpdateRequest;
+import com.flz.port.DepartmentReadPort;
 import com.flz.port.DepartmentSavePort;
 import com.flz.service.DepartmentCreateService;
 import lombok.RequiredArgsConstructor;
@@ -13,14 +17,34 @@ import org.springframework.stereotype.Service;
 class DepartmentCreateServiceImpl implements DepartmentCreateService {
 
     private final DepartmentSavePort departmentSavePort;
+    private final DepartmentReadPort departmentReadPort;
 
     private final DepartmentCreateRequestToDomainMapper departmentCreateRequestToDomainMapper = DepartmentCreateRequestToDomainMapper.INSTANCE;
 
     @Override
-    public void create(final DepartmentCreateRequest createRequest) {
+    public void create(DepartmentCreateRequest createRequest) {
+        boolean existsByName = departmentReadPort.existsByName(createRequest.getName());
+        if (existsByName) {
+            throw new DepartmentAlreadyExistsException(createRequest.getName());
+        }
 
-        final Department department = departmentCreateRequestToDomainMapper.map(createRequest);
+        Department department = departmentCreateRequestToDomainMapper.map(createRequest);
         departmentSavePort.save(department);
 
     }
+
+    @Override
+    public void update(Long id, DepartmentUpdateRequest departmentUpdateRequest) {
+        Department department = departmentReadPort.findById(id)
+                .orElseThrow(() -> new DepartmentNotFoundException(id));
+
+        boolean existsByName = departmentReadPort.existsByName(departmentUpdateRequest.getName());
+        if (existsByName) {
+            throw new DepartmentAlreadyExistsException(departmentUpdateRequest.getName());
+        }
+
+        department.setName(departmentUpdateRequest.getName());
+        departmentSavePort.save(department);
+    }
+
 }
