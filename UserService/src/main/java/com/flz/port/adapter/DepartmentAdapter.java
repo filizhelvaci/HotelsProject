3e49622df75da67aa556fa35403793d6ba8c3934
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-11 10:24:01 +0300

    findAll() and existsByName() added. findById updated

--- UserService/src/main/java/com/flz/port/adapter/DepartmentAdapter.java
+++ UserService/src/main/java/com/flz/port/adapter/DepartmentAdapter.java
@@ -8,8 +8,11 @@ import com.flz.port.DepartmentReadPort;
 import com.flz.port.DepartmentSavePort;
 import com.flz.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -23,16 +26,27 @@ class DepartmentAdapter implements DepartmentReadPort, DepartmentSavePort {
 
 
     @Override
-    public Optional<Department> findById(final Long id) {
-        final Optional<DepartmentEntity> departmentEntity = departmentRepository
-                .findById(id);
+    public List<Department> findAll(Integer page, Integer pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        List<DepartmentEntity> departmentEntities = departmentRepository.findAll(pageable).getContent();
+        return departmentEntityToDomainMapper.map(departmentEntities);
+    }
+
+    @Override
+    public Optional<Department> findById(Long id) {
+        Optional<DepartmentEntity> departmentEntity = departmentRepository.findById(id);
         return departmentEntity.map(departmentEntityToDomainMapper::map);
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return departmentRepository.existsByName(name);
     }
 
     @Override
     public void save(final Department department) {
         final DepartmentEntity departmentEntity = departmentToEntityMapper.map(department);
         departmentRepository.save(departmentEntity);
-        departmentEntityToDomainMapper.map(departmentEntity);
     }
+
 }
