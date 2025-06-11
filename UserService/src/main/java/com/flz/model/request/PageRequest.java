dc162a75081a1f059f9503a7b117c986937190c6
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-11 10:28:01 +0300

    PageRequest created

---
+++ UserService/src/main/java/com/flz/model/request/PageRequest.java
@@ -0,0 +1,20 @@
+package com.flz.model.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@Setter
+public final class PageRequest {
+
+    @Range(min = 1)
+    @NotNull
+    private Integer page;
+
+    @NotNull
+    @Range(min = 1, max = 15)
+    private Integer pageSize;
+
+}
