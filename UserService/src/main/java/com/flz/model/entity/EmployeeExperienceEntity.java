b36abafea15ec601b53612be1a7822ca5c2990a1
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-15 15:08:28 +0300

    EmployeeExperienceEntity created

---
+++ UserService/src/main/java/com/flz/model/entity/EmployeeExperienceEntity.java
@@ -0,0 +1,71 @@
+package com.flz.model.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ru_employee_experience")
+public class EmployeeExperienceEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "salary")
+    private BigDecimal salary;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    /**
+     * ----------------------------------------------------------------
+     * EmployeeExperienceEntity      PositionEntity
+     * M                       1
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "position_id", referencedColumnName = "id")
+    private PositionEntity position;
+
+    /**
+     * ----------------------------------------------------------------
+     * EmployeeExperienceEntity      EmployeeEntity
+     * M                       1
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", referencedColumnName = "id", nullable = false)
+    private EmployeeEntity employee;
+
+    /**
+     * ----------------------------------------------------------------
+     * EmployeeExperienceEntity      EmployeeEntity
+     * M                       1
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supervisor_id", referencedColumnName = "id", nullable = false)
+    private EmployeeEntity supervisor;
+}
