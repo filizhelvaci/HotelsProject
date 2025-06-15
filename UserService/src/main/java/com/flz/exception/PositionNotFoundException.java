cd8e694bb867fa778c28a7487c4474bf2c257ff8
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-15 09:52:29 +0300

    PositionEntityToDomainMapper created

---
+++ UserService/src/main/java/com/flz/exception/PositionNotFoundException.java
@@ -0,0 +1,14 @@
+package com.flz.exception;
+
+import java.io.Serial;
+
+public final class PositionNotFoundException extends AbstractNotFoundException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public PositionNotFoundException(Long id) {
+
+        super("This Department not found ID = " + id);
+    }
+}
