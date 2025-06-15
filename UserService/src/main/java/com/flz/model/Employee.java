10e9d37c318a36453e8390fc2cad1a730ee7546f
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-15 16:17:13 +0300

    Employee Domain created

---
+++ UserService/src/main/java/com/flz/model/Employee.java
@@ -0,0 +1,30 @@
+package com.flz.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Employee extends BaseDomainModel {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String identityNumber;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private LocalDate birthDate;
+    private String gender;
+    private String nationality;
+
+    private List<EmployeeExperience> experiences;
+
+}
