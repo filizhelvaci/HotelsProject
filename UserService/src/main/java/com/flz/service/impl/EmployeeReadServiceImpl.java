e606fdff8690758e8ec4ca10686b833ab0dedf6a
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-17 13:12:12 +0300

    findAll(), findSummaryAll() added to EmployeeReadServiceImpl

--- UserService/src/main/java/com/flz/service/impl/EmployeeReadServiceImpl.java
+++ UserService/src/main/java/com/flz/service/impl/EmployeeReadServiceImpl.java
@@ -2,10 +2,14 @@ package com.flz.service.impl;
 
 import com.flz.exception.EmployeeNotFoundException;
 import com.flz.model.Employee;
+import com.flz.model.mapper.EmployeeToEmployeeSummaryResponseMapper;
+import com.flz.model.response.EmployeeSummaryResponse;
 import com.flz.port.EmployeeReadPort;
 import com.flz.service.EmployeeReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +17,23 @@ public class EmployeeReadServiceImpl implements EmployeeReadService {
 
     private final EmployeeReadPort employeeReadPort;
 
+    private final EmployeeToEmployeeSummaryResponseMapper employeeToEmployeeSummaryResponseMapper;
+
     @Override
     public Employee findById(Long id) {
         return employeeReadPort.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @Override
+    public List<EmployeeSummaryResponse> findSummaryAll() {
+        List<Employee> employee = employeeReadPort.findSummaryAll();
+        return employeeToEmployeeSummaryResponseMapper.map(employee);
+    }
+
+    @Override
+    public List<Employee> findAll(Integer page, Integer pageSize) {
+
+        return employeeReadPort.findAll(page, pageSize);
     }
 
 
