a425da61dcccaf1c1fec131497545aacc33e5b8a
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-17 13:11:16 +0300

    findAll(), findSummaryAll() added to EmployeeReadPort

--- UserService/src/main/java/com/flz/port/EmployeeReadPort.java
+++ UserService/src/main/java/com/flz/port/EmployeeReadPort.java
@@ -2,9 +2,16 @@ package com.flz.port;
 
 import com.flz.model.Employee;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeReadPort {
 
     Optional<Employee> findById(Long id);
+
+    List<Employee> findAll(Integer page, Integer pageSize);
+
+    List<Employee> findSummaryAll();
+
+    boolean existsByIdentity(String identity);
 }
