74c46e854c31bf35d2d825890d9e6b61fcb52930
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-17 22:36:14 +0300

    EmployeeExperienceEntityToDomainMapper created

---
+++ UserService/src/main/java/com/flz/model/mapper/EmployeeExperienceEntityToDomainMapper.java
@@ -0,0 +1,12 @@
+package com.flz.model.mapper;
+
+import com.flz.model.EmployeeExperience;
+import com.flz.model.entity.EmployeeExperienceEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface EmployeeExperienceEntityToDomainMapper extends BaseMapper<EmployeeExperienceEntity, EmployeeExperience> {
+
+    EmployeeExperienceEntityToDomainMapper INSTANCE = Mappers.getMapper(EmployeeExperienceEntityToDomainMapper.class);
+}
