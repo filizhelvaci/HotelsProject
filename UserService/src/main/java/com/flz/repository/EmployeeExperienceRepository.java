99fdf160e8a14ea848120ed6b05667f6e3a5ee68
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-18 19:15:51 +0300

    EmployeeExperienceRepository created and developed

--- UserService/src/main/java/com/flz/repository/EmployeeExperienceRepository.java
+++ UserService/src/main/java/com/flz/repository/EmployeeExperienceRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeExperienceRepository extends JpaRepository<EmployeeExperienceEntity, Integer> {
 
     boolean existsByEmployeeIdAndPositionIdAndStartDate(Long employeeId, Long positionId, LocalDate startDate);
 
-    EmployeeExperienceEntity findTopByEmployeeIdOrderByStartDateDesc(Long id);
+    Optional<EmployeeExperienceEntity> findTopByEmployeeIdOrderByStartDateDesc(Long employeeId);
 
 }
