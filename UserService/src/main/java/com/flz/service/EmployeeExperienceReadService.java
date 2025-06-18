3e540d12c41f26ce917c26252b26150fa688f2ca
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-18 19:12:42 +0300

    page and pageSize removed in findAll()

--- UserService/src/main/java/com/flz/service/EmployeeExperienceReadService.java
+++ UserService/src/main/java/com/flz/service/EmployeeExperienceReadService.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public interface EmployeeExperienceReadService {
 
-    EmployeeExperience findById(Long id);
-
-    List<EmployeeExperience> findAll(Integer page, Integer size);
+    List<EmployeeExperience> findAll();
 
 }
