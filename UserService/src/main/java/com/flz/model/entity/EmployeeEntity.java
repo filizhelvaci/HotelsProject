5a2dd8de0132601afa7e6802807d913c09e5c101
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-21 09:02:17 +0300

    EmployeeEntity and EmployeeExperience dependence updated

--- UserService/src/main/java/com/flz/model/entity/EmployeeEntity.java
+++ UserService/src/main/java/com/flz/model/entity/EmployeeEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -62,7 +63,7 @@ public class EmployeeEntity extends BaseEntity {
     @Column(name = "nationality")
     private String nationality;
 
-    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<EmployeeExperienceEntity> experiences;
 
 }
