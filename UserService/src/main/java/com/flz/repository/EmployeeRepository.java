3f875f37ad9cab788cdd86bdd64f0e05488c4ebd
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-23 09:31:11 +0300

    @Query added to EmployeeRepository for EmployeeSummaryResponse

--- UserService/src/main/java/com/flz/repository/EmployeeRepository.java
+++ UserService/src/main/java/com/flz/repository/EmployeeRepository.java
@@ -1,13 +1,19 @@
 package com.flz.repository;
 
 import com.flz.model.entity.EmployeeEntity;
+import com.flz.model.response.EmployeeSummaryResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
     boolean existsByIdentityNumber(String identityNumber);
 
+    @Query("SELECT new com.flz.model.response.EmployeeSummaryResponse(e.id, e.firstName, e.lastName) FROM EmployeeEntity e")
+    List<EmployeeSummaryResponse> findEmployeeSummaries();
 
 }
