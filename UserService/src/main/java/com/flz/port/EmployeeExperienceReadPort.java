6454be51eaa67451f65149bf790e589424962432
filizhelvaci <filizhelvaci82@gmail.com>
2025-06-23 09:24:35 +0300

    findAll() updated

--- UserService/src/main/java/com/flz/port/EmployeeExperienceReadPort.java
+++ UserService/src/main/java/com/flz/port/EmployeeExperienceReadPort.java
@@ -1,6 +1,7 @@
 package com.flz.port;
 
 import com.flz.model.EmployeeExperience;
+import com.flz.model.response.EmployeeExperienceResponse;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -8,9 +9,9 @@ import java.util.Optional;
 
 public interface EmployeeExperienceReadPort {
 
-    List<EmployeeExperience> findAll();
-
     Optional<EmployeeExperience> findTopByEmployeeIdOrderByStartDateDesc(Long id);
+
+    List<EmployeeExperienceResponse> findAllByEmployee_Id(Long employeeId);
 
     boolean existsByEmployeeIdAndPositionIdAndStartDate(Long employeeId, Long positionId, LocalDate startDate);
 
