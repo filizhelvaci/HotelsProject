28500e4b52e53e61690b34a59a5e7ac2fcbaae05
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-05 08:59:44 +0300

    @Builder added to DepartmentUpdateRequest

--- UserService/src/main/java/com/flz/model/request/DepartmentUpdateRequest.java
+++ UserService/src/main/java/com/flz/model/request/DepartmentUpdateRequest.java
@@ -3,12 +3,14 @@ package com.flz.model.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DepartmentUpdateRequest {
