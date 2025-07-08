f428a446299f781afc479aa66ca780d34a55481e
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-08 14:54:29 +0300

    PositionAdapterTest findSummaryAll() methods tests wrote

--- UserService/src/test/java/com/flz/port/adapter/PositionAdapterTest.java
+++ UserService/src/test/java/com/flz/port/adapter/PositionAdapterTest.java
@@ -1,6 +1,7 @@
 package com.flz.port.adapter;
 
 import com.flz.BaseTest;
+import com.flz.model.Department;
 import com.flz.model.Position;
 import com.flz.model.entity.DepartmentEntity;
 import com.flz.model.entity.PositionEntity;
@@ -20,6 +21,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 class PositionAdapterTest extends BaseTest {
@@ -46,7 +48,6 @@ class PositionAdapterTest extends BaseTest {
 
         //When
         List<PositionEntity> mockPositionEntities = getPositionEntities();
-
         Pageable mockPageable = PageRequest.of(mockPage - 1, mockPageSize);
 
         Page<PositionEntity> mockPositionEntitiesPage = new PageImpl<>(mockPositionEntities);
@@ -86,6 +87,49 @@ class PositionAdapterTest extends BaseTest {
         Mockito.verify(positionRepository, Mockito.times(1))
                 .findAll(mockPageable);
     }
+
+    /**
+     * {@link PositionAdapter#findSummaryAll()}
+     */
+    @Test
+    void whenCalledAllSummaryPosition_thenReturnListOfPositionSummariesResponse() {
+
+        //When
+        List<PositionEntity> mockPositionEntities = getPositionEntities();
+
+        Mockito.when(positionRepository.findAll())
+                .thenReturn(mockPositionEntities);
+
+        //Then
+        List<Position> mockPositions = positionEntityToDomainMapper
+                .map(mockPositionEntities);
+        List<Position> result = adapter.findSummaryAll();
+        Assertions.assertEquals(mockPositions.size(), result.size());
+
+        //Verify
+        Mockito.verify(positionRepository, Mockito.times(1))
+                .findAll();
+    }
+
+
+    @Test
+    void whenCalledAllSummaryPositionsIfPositionListIsEmpty_thenReturnEmptyList() {
+
+        //When
+        Mockito.when(positionRepository.findAll())
+                .thenReturn(Collections.emptyList());
+
+        //Then
+        List<Position> positions = adapter.findSummaryAll();
+
+        Assertions.assertEquals(0, positions.size());
+        Assertions.assertTrue(positions.isEmpty());
+
+        //Verify
+        Mockito.verify(positionRepository, Mockito.times(1))
+                .findAll();
+    }
+
 
     private static PositionEntity getPositionEntity(Long mockId) {
         return PositionEntity.builder()
@@ -146,6 +190,52 @@ class PositionAdapterTest extends BaseTest {
                                 .build())
                         .status(PositionStatus.ACTIVE)
                         .createdBy("SYSTEM")
+                        .createdAt(LocalDateTime.now())
+                        .build());
+    }
+
+    private static List<Position> getPositions() {
+        return List.of(
+                Position.builder()
+                        .id(11L)
+                        .name("Test1")
+                        .department(Department.builder()
+                                .id(1L)
+                                .name("Test1Department")
+                                .status(DepartmentStatus.ACTIVE)
+                                .createdAt(LocalDateTime.now())
+                                .createdUser("TestSystem")
+                                .build())
+                        .status(PositionStatus.ACTIVE)
+                        .createdUser("SYSTEM")
+                        .createdAt(LocalDateTime.now())
+                        .build(),
+                Position.builder()
+                        .id(12L)
+                        .name("Test2")
+                        .department(Department.builder()
+                                .id(1L)
+                                .name("Test2Department")
+                                .status(DepartmentStatus.ACTIVE)
+                                .createdAt(LocalDateTime.now())
+                                .createdUser("TestSystem")
+                                .build())
+                        .status(PositionStatus.ACTIVE)
+                        .createdUser("SYSTEM")
+                        .createdAt(LocalDateTime.now())
+                        .build(),
+                Position.builder()
+                        .id(13L)
+                        .name("Test3")
+                        .department(Department.builder()
+                                .id(2L)
+                                .name("Test3Department")
+                                .status(DepartmentStatus.ACTIVE)
+                                .createdAt(LocalDateTime.now())
+                                .createdUser("TestSystem")
+                                .build())
+                        .status(PositionStatus.ACTIVE)
+                        .createdUser("SYSTEM")
                         .createdAt(LocalDateTime.now())
                         .build());
     }
