78be70d43190d8633051d197154b5cc8146f86aa
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-13 09:33:46 +0300

    DepartmentTestCleaner class was written that allows deletion of records containing the word test.

---
+++ UserService/src/test/java/com/flz/cleaner/DepartmentTestCleaner.java
@@ -0,0 +1,22 @@
+package com.flz.cleaner;
+
+import com.flz.repository.DepartmentRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class DepartmentTestCleaner {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentTestCleaner(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Transactional
+    public void cleanTestDepartments() {
+
+        departmentRepository.deleteAllByNameContainingIgnoreCase("test");
+    }
+}
