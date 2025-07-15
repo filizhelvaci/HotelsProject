69dc4e447ba2c59b95e5a16c081b931446e19bdf
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-15 13:06:50 +0300

    EmployeeExperienceCreateServiceImpl create() methods updated

--- UserService/src/main/java/com/flz/service/impl/EmployeeExperienceCreateServiceImpl.java
+++ UserService/src/main/java/com/flz/service/impl/EmployeeExperienceCreateServiceImpl.java
@@ -1,12 +1,16 @@
 package com.flz.service.impl;
 
 import com.flz.exception.EmployeeExperienceAlreadyExistsException;
+import com.flz.exception.EmployeeNotFoundException;
+import com.flz.exception.PositionNotFoundException;
 import com.flz.model.Employee;
 import com.flz.model.EmployeeExperience;
 import com.flz.model.Position;
 import com.flz.model.request.EmployeeExperienceCreateRequest;
 import com.flz.port.EmployeeExperienceReadPort;
 import com.flz.port.EmployeeExperienceSavePort;
+import com.flz.port.EmployeeReadPort;
+import com.flz.port.PositionReadPort;
 import com.flz.service.EmployeeExperienceCreateService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -21,17 +25,35 @@ class EmployeeExperienceCreateServiceImpl implements EmployeeExperienceCreateSer
 
     private final EmployeeExperienceSavePort employeeExperienceSavePort;
     private final EmployeeExperienceReadPort employeeExperienceReadPort;
+    private final EmployeeReadPort employeeReadPort;
+    private final PositionReadPort positionReadPort;
+
 
     @Override
     @Transactional
     public void create(Long employeeId, EmployeeExperienceCreateRequest createRequest) {
 
-        boolean existsByEmployeeIdAndPositionIdAndStartDate = employeeExperienceReadPort.existsByEmployeeIdAndPositionIdAndStartDate(employeeId, createRequest.getPositionId(), createRequest.getStartDate());
+        Employee employee = employeeReadPort.findById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFoundException(employeeId));
+
+        Employee employeeSupervisor = employeeReadPort.findById(createRequest.getSupervisorId())
+                .orElseThrow(() -> new EmployeeNotFoundException(createRequest.getSupervisorId()));
+
+        Position position = positionReadPort.findById(createRequest.getPositionId())
+                .orElseThrow(() -> new PositionNotFoundException(createRequest.getPositionId()));
+
+        boolean existsByEmployeeIdAndPositionIdAndStartDate = employeeExperienceReadPort
+                .existsByEmployeeIdAndPositionIdAndStartDate(
+                        employeeId,
+                        createRequest.getPositionId(),
+                        createRequest.getStartDate());
+
         if (existsByEmployeeIdAndPositionIdAndStartDate) {
             throw new EmployeeExperienceAlreadyExistsException(employeeId);
         }
 
-        Optional<EmployeeExperience> lastExperience = employeeExperienceReadPort.findTopByEmployeeIdOrderByStartDateDesc(employeeId);
+        Optional<EmployeeExperience> lastExperience = employeeExperienceReadPort
+                .findTopByEmployeeIdOrderByStartDateDesc(employeeId);
         if (lastExperience.isPresent()) {
             EmployeeExperience lastEmployeeExperience = lastExperience.get();
             LocalDate newEndDate = createRequest.getStartDate().minusDays(1);
@@ -42,9 +64,10 @@ class EmployeeExperienceCreateServiceImpl implements EmployeeExperienceCreateSer
         EmployeeExperience employeeExperience = EmployeeExperience.builder()
                 .salary(createRequest.getSalary())
                 .startDate(createRequest.getStartDate())
-                .position(Position.builder().id(createRequest.getPositionId()).build())
-                .employee(Employee.builder().id(employeeId).build())
-                .supervisor(Employee.builder().id(createRequest.getSupervisorId()).build()).build();
+                .position(position)
+                .employee(employee)
+                .supervisor(employeeSupervisor)
+                .build();
         employeeExperienceSavePort.save(employeeExperience);
     }
 
