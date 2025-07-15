75414b5f028fd57deebf940519a3530411f244cb
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-15 13:01:36 +0300

    @Mapping fields removed in mapper files

--- UserService/src/main/java/com/flz/model/mapper/EmployeeEntityToDomainMapper.java
+++ UserService/src/main/java/com/flz/model/mapper/EmployeeEntityToDomainMapper.java
@@ -3,15 +3,11 @@ package com.flz.model.mapper;
 import com.flz.model.Employee;
 import com.flz.model.entity.EmployeeEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface EmployeeEntityToDomainMapper extends BaseMapper<EmployeeEntity, Employee> {
 
     EmployeeEntityToDomainMapper INSTANCE = Mappers.getMapper(EmployeeEntityToDomainMapper.class);
-
-    @Mapping(target = "experiences", ignore = true)
-    Employee map(EmployeeEntity source);
 
 }
