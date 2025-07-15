955bea955e0a675fd26650bdc756cd14cad67b6e
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-15 14:37:12 +0300

    EmployeeAdapter findSummaryAll() methods tests written

--- UserService/src/test/java/com/flz/port/adapter/EmployeeAdapterTest.java
+++ UserService/src/test/java/com/flz/port/adapter/EmployeeAdapterTest.java
@@ -4,6 +4,7 @@ import com.flz.BaseTest;
 import com.flz.model.Employee;
 import com.flz.model.entity.EmployeeEntity;
 import com.flz.model.enums.Gender;
+import com.flz.model.response.EmployeeSummaryResponse;
 import com.flz.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,8 @@ import org.mockito.Mockito;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 class EmployeeAdapterTest extends BaseTest {
@@ -22,6 +25,62 @@ class EmployeeAdapterTest extends BaseTest {
 
     @Mock
     EmployeeRepository employeeRepository;
+
+
+    /**
+     * {@link EmployeeAdapter#findSummaryAll()}
+     */
+    @Test
+    void whenCalledAllSummaryEmployee_thenReturnListOfEmployeeSummariesResponse() {
+
+        //When
+        List<EmployeeSummaryResponse> mockEmployeeSummaryResponse = List.of(
+                EmployeeSummaryResponse.builder()
+                        .id(1L)
+                        .firstName("John")
+                        .lastName("Doe")
+                        .build(),
+                EmployeeSummaryResponse.builder()
+                        .id(2L)
+                        .firstName("Jane")
+                        .lastName("Doe")
+                        .build(),
+                EmployeeSummaryResponse.builder()
+                        .id(3L)
+                        .firstName("Bob")
+                        .lastName("Joe")
+                        .build());
+
+        Mockito.when(employeeRepository.findEmployeeSummaries())
+                .thenReturn(mockEmployeeSummaryResponse);
+
+        //Then
+        List<EmployeeSummaryResponse> result = employeeAdapter.findSummaryAll();
+        Assertions.assertEquals(mockEmployeeSummaryResponse.size(), result.size());
+
+        //Verify
+        Mockito.verify(employeeRepository, Mockito.times(1))
+                .findEmployeeSummaries();
+    }
+
+
+    @Test
+    void whenCalledAllSummaryEmployeeIfEmployeeListIsEmpty_thenReturnEmptyList() {
+
+        //When
+        Mockito.when(employeeRepository.findEmployeeSummaries())
+                .thenReturn(Collections.emptyList());
+
+        //Then
+        List<EmployeeSummaryResponse> responses = employeeAdapter.findSummaryAll();
+
+        Assertions.assertEquals(0, responses.size());
+        Assertions.assertTrue(responses.isEmpty());
+
+        //Verify
+        Mockito.verify(employeeRepository, Mockito.times(1))
+                .findEmployeeSummaries();
+    }
 
     /**
      * {@link EmployeeAdapter#findById(Long)}
@@ -97,5 +156,48 @@ class EmployeeAdapterTest extends BaseTest {
                 .nationality("TC")
                 .phoneNumber("05465321456")
                 .build();
+    }
+
+    private static List<EmployeeEntity> getEmployeeEntities() {
+        return List.of(
+                EmployeeEntity.builder()
+                        .id(1L)
+                        .firstName("test first name 1")
+                        .lastName("test last name 1")
+                        .address("test address 1")
+                        .birthDate(LocalDate.parse("2000-01-01"))
+                        .createdBy("SYSTEM")
+                        .createdAt(LocalDateTime.now())
+                        .email("test1@gmail.com")
+                        .gender(Gender.FEMALE)
+                        .nationality("TC")
+                        .phoneNumber("05465321456")
+                        .build(),
+                EmployeeEntity.builder()
+                        .id(2L)
+                        .firstName("test first name 2")
+                        .lastName("test last name 2 ")
+                        .address("test address 2")
+                        .birthDate(LocalDate.parse("2000-02-02"))
+                        .createdBy("SYSTEM")
+                        .createdAt(LocalDateTime.now())
+                        .email("test2@gmail.com")
+                        .gender(Gender.FEMALE)
+                        .nationality("TC")
+                        .phoneNumber("05465321465")
+                        .build(),
+                EmployeeEntity.builder()
+                        .id(3L)
+                        .firstName("test first name 3")
+                        .lastName("test last name 3")
+                        .address("test address 3")
+                        .birthDate(LocalDate.parse("2000-03-03"))
+                        .createdBy("SYSTEM")
+                        .createdAt(LocalDateTime.now())
+                        .email("test3@gmail.com")
+                        .gender(Gender.FEMALE)
+                        .nationality("TC")
+                        .phoneNumber("05465321499")
+                        .build());
     }
 }
