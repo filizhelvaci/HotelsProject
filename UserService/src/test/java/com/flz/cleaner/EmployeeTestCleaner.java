8ba1fe3c5138dacf41416fc51381631554faf5af
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-23 15:07:59 +0300

    Created class to delete test data

---
+++ UserService/src/test/java/com/flz/cleaner/EmployeeTestCleaner.java
@@ -0,0 +1,20 @@
+package com.flz.cleaner;
+
+import com.flz.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeTestCleaner {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeTestCleaner(final EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Transactional
+    public void cleanEmployees() {
+        employeeRepository.deleteAllByFirstNameContainingIgnoreCase("test");
+    }
+}
