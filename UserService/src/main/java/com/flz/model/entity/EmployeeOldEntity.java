13d4420a2be84d349b2373978230ddccb58c8c5b
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-25 10:41:04 +0300

    EmployeeOldEntity created

---
+++ UserService/src/main/java/com/flz/model/entity/EmployeeOldEntity.java
@@ -0,0 +1,55 @@
+package com.flz.model.entity;
+
+import com.flz.model.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ru_employee_old")
+public class EmployeeOldEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "identity_number")
+    private String identityNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "nationality")
+    private String nationality;
+
+}
