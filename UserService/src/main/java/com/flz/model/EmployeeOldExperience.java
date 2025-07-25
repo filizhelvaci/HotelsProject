b2df7b0c25510f4cfd04c7869b5a9f622446b0ce
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-25 10:42:28 +0300

    EmployeeOldExperience created

---
+++ UserService/src/main/java/com/flz/model/EmployeeOldExperience.java
@@ -0,0 +1,26 @@
+package com.flz.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class EmployeeOldExperience extends BaseDomainModel {
+
+    private Long id;
+    private BigDecimal salary;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private Position position;
+    private Employee employee;
+    private Employee supervisor;
+
+}
