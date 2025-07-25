20837fed2bf10b30b84c94f60d05221dd8c82eab
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-25 16:11:11 +0300

    EmployeeOldExperienceResponse ceated

---
+++ UserService/src/main/java/com/flz/model/response/EmployeeOldExperienceResponse.java
@@ -0,0 +1,25 @@
+package com.flz.model.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class EmployeeOldExperienceResponse {
+
+
+    private Long id;
+    private BigDecimal salary;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String positionName;
+    private String departmentName;
+    private String supervisorName;
+
+}
