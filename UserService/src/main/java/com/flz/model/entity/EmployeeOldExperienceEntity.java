704e30a5a3d5bf792bd66a76a0fd63c4434463f5
filizhelvaci <filizhelvaci82@gmail.com>
2025-07-25 10:40:43 +0300

    EmployeeOldExperienceEntity created

---
+++ UserService/src/main/java/com/flz/model/entity/EmployeeOldExperienceEntity.java
@@ -0,0 +1,63 @@
+package com.flz.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ru_employee_old_experience")
+public class EmployeeOldExperienceEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "salary")
+    private BigDecimal salary;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    /**
+     * ----------------------------------------------------------------
+     * EmployeeExperienceEntity      PositionEntity
+     * M                       1
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "position_id", referencedColumnName = "id")
+    private PositionEntity position;
+
+    /**
+     * ----------------------------------------------------------------
+     * EmployeeExperienceEntity      EmployeeEntity
+     * M                       1
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "employee_id", referencedColumnName = "id", nullable = false)
+    private EmployeeEntity employee;
+
+    /**
+     * ----------------------------------------------------------------
+     * EmployeeExperienceEntity      EmployeeEntity
+     * M                       1
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "supervisor_id", referencedColumnName = "id", nullable = false)
+    private EmployeeEntity supervisor;
+
+}
