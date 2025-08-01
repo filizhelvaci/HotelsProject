f103386b24de08e5378c3fdf73880107bef20bce
filizhelvaci <filizhelvaci82@gmail.com>
2025-08-01 11:52:49 +0300

    EmployeeOldExperienceAdapter package-private made

--- UserService/src/main/java/com/flz/port/adapter/EmployeeOldExperienceAdapter.java
+++ UserService/src/main/java/com/flz/port/adapter/EmployeeOldExperienceAdapter.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class EmployeeOldExperienceAdapter implements EmployeeOldExperienceReadPort, EmployeeOldExperienceSavePort {
+class EmployeeOldExperienceAdapter implements EmployeeOldExperienceReadPort, EmployeeOldExperienceSavePort {
 
     private final EmployeeOldExperienceRepository employeeOldExperienceRepository;
 
