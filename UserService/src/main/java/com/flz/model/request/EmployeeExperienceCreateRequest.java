70f24aeadc4b688f2536b711879e5c187066e9d7
filizhelvaci <filizhelvaci82@gmail.com>
2025-08-02 13:50:14 +0300

    Builder anotation added

--- UserService/src/main/java/com/flz/model/request/EmployeeExperienceCreateRequest.java
+++ UserService/src/main/java/com/flz/model/request/EmployeeExperienceCreateRequest.java
@@ -3,6 +3,7 @@ package com.flz.model.request;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeExperienceCreateRequest {
