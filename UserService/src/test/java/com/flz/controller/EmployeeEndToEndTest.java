fa28510f5ae4f1d4e529e75367d4ee16bef0fe52
filizhelvaci <filizhelvaci82@gmail.com>
2025-08-06 12:17:32 +0300

    EmployeeEndToEndTest findById() methods test updated with EmployeeExperiences

--- UserService/src/test/java/com/flz/controller/EmployeeEndToEndTest.java
+++ UserService/src/test/java/com/flz/controller/EmployeeEndToEndTest.java
@@ -2,9 +2,15 @@ package com.flz.controller;
 
 import com.flz.BaseEndToEndTest;
 import com.flz.model.Employee;
+import com.flz.model.EmployeeExperience;
+import com.flz.model.Position;
 import com.flz.model.enums.Gender;
+import com.flz.model.mapper.EmployeeExperienceToResponseMapper;
 import com.flz.model.request.EmployeeCreateRequest;
 import com.flz.model.request.EmployeeUpdateRequest;
+import com.flz.model.response.EmployeeDetailsResponse;
+import com.flz.port.EmployeeExperienceReadPort;
+import com.flz.port.EmployeeExperienceSavePort;
 import com.flz.port.EmployeeReadPort;
 import com.flz.port.EmployeeSavePort;
 import org.junit.jupiter.api.Assertions;
@@ -29,10 +35,16 @@ class EmployeeEndToEndTest extends BaseEndToEndTest {
     @Autowired
     private EmployeeSavePort employeeSavePort;
 
+    private final EmployeeExperienceToResponseMapper employeeExperienceToResponseMapper = EmployeeExperienceToResponseMapper.INSTANCE;
+    @Autowired
+    private EmployeeExperienceSavePort employeeExperienceSavePort;
+    @Autowired
+    private EmployeeExperienceReadPort employeeExperienceReadPort;
+
     private static final String BASE_PATH = "/api/v1";
 
     @Test
-    void whenFindByIdEmployee_thenReturnEmployeeVerifyContent() throws Exception {
+    void whenFindByIdEmployeewWithExperiences_thenReturnEmployeeVerifyContent() throws Exception {
 
         //Initialize
         Employee employee = Employee.builder()
@@ -46,11 +58,51 @@ class EmployeeEndToEndTest extends BaseEndToEndTest {
                 .gender(Gender.MALE)
                 .nationality("TR")
                 .build();
-
         Employee savedEmployee = employeeSavePort.save(employee);
 
         //Given
         Long employeeId = savedEmployee.getId();
+
+        Position position = Position.builder()
+                .id(10L)
+                .name("Developer")
+                .build();
+
+        Position position2 = Position.builder()
+                .id(11L)
+                .name("Team Leader")
+                .build();
+
+        Employee supervisor = Employee.builder()
+                .id(5L)
+                .identityNumber("98765432109")
+                .firstName("Eric")
+                .lastName("Smith")
+                .gender(Gender.MALE)
+                .nationality("USA")
+                .address("New York")
+                .phoneNumber("05328529674")
+                .build();
+
+
+        EmployeeExperience employeeExperience = EmployeeExperience.builder()
+                .salary(BigDecimal.valueOf(10000))
+                .startDate(LocalDate.parse("2000-10-01"))
+                .endDate(LocalDate.parse("2001-10-31"))
+                .position(position)
+                .employee(savedEmployee)
+                .supervisor(supervisor)
+                .build();
+        employeeExperienceSavePort.save(employeeExperience);
+
+        EmployeeExperience employeeExperience2 = EmployeeExperience.builder()
+                .salary(BigDecimal.valueOf(20000))
+                .startDate(LocalDate.parse("2002-10-01"))
+                .position(position2)
+                .employee(savedEmployee)
+                .supervisor(supervisor)
+                .build();
+        employeeExperienceSavePort.save(employeeExperience2);
 
         //When
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
@@ -87,7 +139,72 @@ class EmployeeEndToEndTest extends BaseEndToEndTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences")
                         .isArray())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences.length()")
-                        .value(0));
+                        .value(2))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].id")
+                        .value(16))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].salary")
+                        .value(10000.00))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].startDate")
+                        .value("2000-10-01"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].endDate")
+                        .value("2001-10-31"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].positionId")
+                        .value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].positionName")
+                        .value("Konsiyerj"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].supervisorId")
+                        .value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[0].supervisorName")
+                        .value("Mustafa Şahin"))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].id")
+                        .value(17))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].salary")
+                        .value(20000.00))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].startDate")
+                        .value("2002-10-01"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].endDate")
+                        .doesNotExist())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].positionId")
+                        .value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].positionName")
+                        .value("Kat Görevlisi"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].supervisorId")
+                        .value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.experiences[1].supervisorName")
+                        .value("Mustafa Şahin"));
+
+        //Verify
+        Employee employeeFromDatabase = employeeReadPort.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + employeeId));
+
+        EmployeeDetailsResponse employeeDetailsResponse = EmployeeDetailsResponse.builder()
+                .employee(employeeFromDatabase)
+                .experiences(employeeExperienceToResponseMapper.map(employeeExperienceReadPort.findAllByEmployeeId(employeeId)))
+                .build();
+
+        Assertions.assertNotNull(employeeDetailsResponse);
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences());
+        Assertions.assertNotNull(employeeDetailsResponse.getEmployee().getFirstName());
+        Assertions.assertNotNull(employeeDetailsResponse.getEmployee().getLastName());
+        Assertions.assertNotNull(employeeDetailsResponse.getEmployee().getIdentityNumber());
+        Assertions.assertNotNull(employeeDetailsResponse.getEmployee().getPhoneNumber());
+        Assertions.assertNotNull(employeeDetailsResponse.getEmployee().getBirthDate());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(0).getId());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(0).getStartDate());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(0).getEndDate());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(0).getPositionId());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(0).getSupervisorName());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(1).getStartDate());
+        Assertions.assertNull(employeeDetailsResponse.getExperiences().get(1).getEndDate());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(1).getPositionName());
+        Assertions.assertNotNull(employeeDetailsResponse.getExperiences().get(1).getSupervisorName());
+        Assertions.assertEquals(savedEmployee.getIdentityNumber(), employeeFromDatabase.getIdentityNumber());
+        Assertions.assertEquals(savedEmployee.getEmail(), employeeFromDatabase.getEmail());
+        Assertions.assertEquals(savedEmployee.getPhoneNumber(), employeeFromDatabase.getPhoneNumber());
+        Assertions.assertEquals(savedEmployee.getBirthDate(), employeeFromDatabase.getBirthDate());
+
     }
 
     @Test
