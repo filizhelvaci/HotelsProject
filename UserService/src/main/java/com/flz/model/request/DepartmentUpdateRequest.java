bd2e24c77f83c59ccbd93f8357875785979697e3
filizhelvaci <filizhelvaci82@gmail.com>
2025-08-12 13:00:16 +0300

    Manager added to DepartmentUpdateRequest

--- UserService/src/main/java/com/flz/model/request/DepartmentUpdateRequest.java
+++ UserService/src/main/java/com/flz/model/request/DepartmentUpdateRequest.java
@@ -1,6 +1,8 @@
 package com.flz.model.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,5 +20,9 @@ public class DepartmentUpdateRequest {
     @NotBlank
     @Size(min = 2, max = 50)
     private String name;
+
+    @NotNull
+    @Positive
+    private Long managerId;
 
 }
