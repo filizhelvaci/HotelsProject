cb3c6f67030b77915cb3c185b2f2f39d15f70671
filizhelvaci <filizhelvaci82@gmail.com>
2025-08-14 19:30:50 +0300

    DepartmentReadServiceImpl findSummaryAll() and FindAll() method tests with manager wrote

--- UserService/src/test/java/com/flz/service/impl/DepartmentReadServiceImplTest.java
+++ UserService/src/test/java/com/flz/service/impl/DepartmentReadServiceImplTest.java
@@ -2,6 +2,9 @@ package com.flz.service.impl;
 
 import com.flz.BaseTest;
 import com.flz.model.Department;
+import com.flz.model.Employee;
+import com.flz.model.enums.DepartmentStatus;
+import com.flz.model.enums.Gender;
 import com.flz.model.mapper.DepartmentToDepartmentSummaryResponseMapper;
 import com.flz.model.response.DepartmentSummaryResponse;
 import com.flz.port.DepartmentReadPort;
@@ -11,7 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
@@ -30,24 +33,63 @@ class DepartmentReadServiceImplTest extends BaseTest {
     @Test
     void whenCalledAllSummaryDepartment_thenReturnListOfDepartmentsSummaryResponse() {
 
+        //Initialize
+        Employee manager = Employee.builder()
+                .id(11L)
+                .firstName("Jonny")
+                .lastName("Deep")
+                .identityNumber("999896314785")
+                .email("jonnyd@example.com")
+                .phoneNumber("05059966565")
+                .address("Malatya")
+                .birthDate(LocalDate.of(1985, 1, 15))
+                .gender(Gender.MALE)
+                .nationality("USA")
+                .build();
+
+        Employee manage2 = Employee.builder()
+                .id(12L)
+                .firstName("George")
+                .lastName("Colony")
+                .identityNumber("888877314785")
+                .email("george@example.com")
+                .phoneNumber("05329911565")
+                .address("Konya")
+                .birthDate(LocalDate.of(1983, 1, 15))
+                .gender(Gender.MALE)
+                .nationality("USA")
+                .build();
+
+        List<Department> mockDepartments = List.of(
+                Department.builder()
+                        .id(21L)
+                        .name("Güvenlik")
+                        .status(DepartmentStatus.ACTIVE)
+                        .manager(manager)
+                        .build(),
+                Department.builder()
+                        .id(22L)
+                        .name("Bilgi Islem")
+                        .status(DepartmentStatus.ACTIVE)
+                        .manager(manage2)
+                        .build());
+
         //When
-        List<Department> mockDepartments = new ArrayList<>();
-
-        Department mockDepartment1 = Department.builder().name("Test1").build();
-        Department mockDepartment2 = Department.builder().name("Test2").build();
-        mockDepartments.add(mockDepartment1);
-        mockDepartments.add(mockDepartment2);
-
         Mockito.when(departmentReadPort.findSummaryAll()).thenReturn(mockDepartments);
 
         List<DepartmentSummaryResponse> mockDepartmentSummaryResponses =
-                DepartmentToDepartmentSummaryResponseMapper.INSTANCE
-                        .map(mockDepartments);
+                DepartmentToDepartmentSummaryResponseMapper.INSTANCE.map(mockDepartments);
 
         //Then
         List<DepartmentSummaryResponse> result = departmentReadServiceImpl
                 .findSummaryAll();
+
         Assertions.assertEquals(mockDepartmentSummaryResponses, result);
+        Assertions.assertEquals(mockDepartments.size(), result.size());
+        Assertions.assertEquals(mockDepartments.get(0).getId(), result.get(0).getId());
+        Assertions.assertEquals(mockDepartments.get(0).getName(), result.get(0).getName());
+        Assertions.assertEquals(mockDepartmentSummaryResponses.get(0).getId(), result.get(0).getId());
+        Assertions.assertEquals(mockDepartmentSummaryResponses.get(0).getName(), result.get(0).getName());
 
         //Verify
         Mockito.verify(departmentReadPort, Mockito.times(1))
@@ -84,25 +126,67 @@ class DepartmentReadServiceImplTest extends BaseTest {
     @Test
     void givenValidPagePageSize_whenCalledAllDepartment_thenReturnListAllOfDepartments() {
 
+        //Initialize
+        Employee manager = Employee.builder()
+                .id(11L)
+                .firstName("Jonny")
+                .lastName("Deep")
+                .identityNumber("999896314785")
+                .email("jonnyd@example.com")
+                .phoneNumber("05059966565")
+                .address("Malatya")
+                .birthDate(LocalDate.of(1985, 1, 15))
+                .gender(Gender.MALE)
+                .nationality("USA")
+                .build();
+
+        Employee manage2 = Employee.builder()
+                .id(12L)
+                .firstName("George")
+                .lastName("Colony")
+                .identityNumber("888877314785")
+                .email("george@example.com")
+                .phoneNumber("05329911565")
+                .address("Konya")
+                .birthDate(LocalDate.of(1983, 1, 15))
+                .gender(Gender.MALE)
+                .nationality("USA")
+                .build();
+
+        List<Department> mockDepartments = List.of(
+                Department.builder()
+                        .id(21L)
+                        .name("Güvenlik")
+                        .status(DepartmentStatus.ACTIVE)
+                        .manager(manager)
+                        .build(),
+                Department.builder()
+                        .id(22L)
+                        .name("Bilgi Islem")
+                        .status(DepartmentStatus.ACTIVE)
+                        .manager(manage2)
+                        .build());
         //Given
         Integer mockPage = 1;
         Integer mockPageSize = 10;
 
         //When
-        List<Department> mockDepartments = new ArrayList<>();
-
-        Department mockDepartment1 = Department.builder().name("Test1").build();
-        Department mockDepartment2 = Department.builder().name("Test2").build();
-        mockDepartments.add(mockDepartment1);
-        mockDepartments.add(mockDepartment2);
-
         Mockito.when(departmentReadPort.findAll(mockPage, mockPageSize))
                 .thenReturn(mockDepartments);
 
         //Then
-        List<Department> mockDepartment = departmentReadServiceImpl
+        List<Department> result = departmentReadServiceImpl
                 .findAll(mockPage, mockPageSize);
-        Assertions.assertEquals(mockDepartments.size(), mockDepartment.size());
+
+        Assertions.assertEquals(mockDepartments.size(), result.size());
+        Assertions.assertEquals(mockDepartments.get(0).getId(), result.get(0).getId());
+        Assertions.assertEquals(mockDepartments.get(0).getName(), result.get(0).getName());
+        Assertions.assertEquals(mockDepartments.get(0).getStatus(), result.get(0).getStatus());
+        Assertions.assertEquals(mockDepartments.get(0).getManager(), result.get(0).getManager());
+        Assertions.assertEquals(mockDepartments.get(1).getId(), result.get(1).getId());
+        Assertions.assertEquals(mockDepartments.get(1).getName(), result.get(1).getName());
+        Assertions.assertEquals(mockDepartments.get(1).getStatus(), result.get(1).getStatus());
+        Assertions.assertEquals(mockDepartments.get(1).getManager(), result.get(1).getManager());
 
         //Verify
         Mockito.verify(departmentReadPort, Mockito.times(1))
