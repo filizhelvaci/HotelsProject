c0c841a95504afabb3ee967b7fe2f3eae93d1c35
filizhelvaci <filizhelvaci82@gmail.com>
2025-09-23 18:05:33 +0300

    ReservationService ReservationsApp updated

--- ReservationsService/src/main/java/com/flz/ReservationsApp.java
+++ ReservationsService/src/main/java/com/flz/ReservationsApp.java
@@ -2,11 +2,10 @@ package com.flz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class ReservationsApp {
+
     public static void main(String[] args) {
 
         SpringApplication.run(ReservationsApp.class, args);
