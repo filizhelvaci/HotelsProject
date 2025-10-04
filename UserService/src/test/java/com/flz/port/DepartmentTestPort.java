71faf96c4b72614daa78343285586b1c60ba1f08
filizhelvaci <filizhelvaci82@gmail.com>
2025-10-04 12:08:47 +0300

    DepartmentTestPort updated for Optional

--- UserService/src/test/java/com/flz/port/DepartmentTestPort.java
+++ UserService/src/test/java/com/flz/port/DepartmentTestPort.java
@@ -2,9 +2,11 @@ package com.flz.port;
 
 import com.flz.model.Department;
 
+import java.util.Optional;
+
 public interface DepartmentTestPort {
 
     Department save(Department department);
 
-    Department findByName(String departmentName);
+    Optional<Department> findByName(String departmentName);
 }
