e689f53dfee3dfa81c93a41c5ef0f6d619fe6d3e
filizhelvaci <filizhelvaci82@gmail.com>
2025-10-19 09:29:53 +0300

    Exception files added to Reservation Service

---
+++ ReservationsService/src/main/java/com/flz/exception/AbstractAlreadyExistsException.java
@@ -0,0 +1,14 @@
+package com.flz.exception;
+
+import java.io.Serial;
+
+public abstract class AbstractAlreadyExistsException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1045882356489907872L;
+
+    protected AbstractAlreadyExistsException(String message) {
+
+        super(message);
+    }
+}
