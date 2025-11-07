fb8cedd43825b07dbf562702cb69255c96f192e8
filizhelvaci <filizhelvaci82@gmail.com>
2025-11-07 13:46:18 +0300

    unused method removed

--- RoomService/src/main/java/com/flz/repository/RoomRepository.java
+++ RoomService/src/main/java/com/flz/repository/RoomRepository.java
@@ -5,13 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RoomRepository extends JpaRepository<RoomEntity, Long>, JpaSpecificationExecutor<RoomEntity> {
 
     boolean existsByNumber(Integer number);
-
-    List<RoomEntity> findByType_Id(Long typeId);
 
 }
